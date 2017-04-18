c0d8c79e513aa56cd8999cd3dd7d547c8412c0c8
Tom Swann <ts.swann@gmail.com>
2017-04-18 16:05:04 +0100

    Adding upaid work flag staging schema

---
+++ migrations/20170418160405_stg_flag_upw.js
@@ -0,0 +1,20 @@
+
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('stg_flag_upw', function (table) {
+    table.increments('id')
+    table.string('row_type')
+    table.string('case_ref_no')
+    table.string('tier_code')
+    table.string('team_code')
+    table.string('om_grade_code')
+    table.string('om_key')
+    table.string('location')
+  }).catch(function (error) {
+    console.log(error)
+    throw error
+  })
+}
+
+exports.down = function (knex, Promise) {
+  knex.schema.dropTable('stg_flag_upw')
+}
