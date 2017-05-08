901d27ed81d0f8ace0c30fe005bb66870ff75b49
Steven Cochrane <s.cochrane@kainos.com>
2017-05-08 16:19:00 +0100

    WMT-417: added app schema for LDU

---
+++ migrations/20170508160924_app_ldu.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('ldu', function (table) {
+    table.increments('id')
+    table.string('description')
+    table.timestamp('effective_from').defaultTo(knex.fn.now())
+    table.timestamp('effective_to')
+  }).catch(function (error) {
+    console.log(error)
+    throw error
+  })
+}
+
+exports.down = function(knex, Promise) {
+  knex.schema.dropTable('ldu')
+}
