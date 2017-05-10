6dc44cc06025210f5b618e5692773fc6bc0e3edf
Steven Cochrane <s.cochrane@kainos.com>
2017-05-10 10:13:35 +0100

    WMT-417: added app schema for team

---
+++ migrations/20170508160911_app_team.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('team', function (table) {
+    table.increments('id')
+    table.bigInteger('ldu_id').references('id').inTable('ldu')
+    table.string('description')
+    table.timestamp('effective_from').defaultTo(knex.fn.now())
+    table.timestamp('effective_to')
+  }).catch(function (error) {
+    console.log(error)
+    throw error
+  })
+}
+
+exports.down = function(knex, Promise) {
+  knex.schema.dropTable('team')
+}
