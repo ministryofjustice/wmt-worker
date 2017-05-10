e4a4af4a1aec3770debb324cbc1ce9fcba3c9f59
Steven Cochrane <s.cochrane@kainos.com>
2017-05-10 20:22:09 +0100

    WMT-417: added tier type, case type and points to all tiers schemas

--- migrations/20170508160141_app_community_tiers.js
+++ migrations/20170508160141_app_community_tiers.js
@@ -3,6 +3,9 @@ exports.up = function(knex, Promise) {
   return knex.schema.withSchema('app').createTable('community_tiers', function (table) {
     table.increments('id')
     table.bigInteger('workload_id').references('id').inTable('workload')
+    table.integer('tier_type').unsigned()
+    table.string('case_type', 5)
+    table.bigInteger('points').unsigned()
   }).catch(function (error) {
     console.log(error)
     throw error
