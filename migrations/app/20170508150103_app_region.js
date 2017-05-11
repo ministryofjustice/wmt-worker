bb4d9413398911ddcc4aca2a432b7235e537e259
Tom Swann <ts.swann@gmail.com>
2017-05-11 19:19:58 +0100

    Added region reference table and FK in LDU

---
+++ migrations/app/20170508150103_app_region.js
@@ -0,0 +1,14 @@
+
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('region', function (table) {
+    table.increments('id')
+    table.string('description')
+  }).catch(function (error) {
+    console.log(error)
+    throw error
+  })
+}
+
+exports.down = function (knex, Promise) {
+  knex.schema.dropTable('region')
+}
--- migrations/app/20170508150220_app_ldu.js
+++ migrations/app/20170508150220_app_ldu.js
@@ -3,6 +3,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('ldu', function (table) {
     table.increments('id')
     table.string('description')
+    table.integer('region_id').unsigned().notNullable().references('region.id')
     table.timestamp('effective_from').defaultTo(knex.fn.now())
     table.timestamp('effective_to')
   }).catch(function (error) {
