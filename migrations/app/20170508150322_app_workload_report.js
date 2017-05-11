52abaac72d72b8345887e259ce2f367a4def49c3
Tom Swann <ts.swann@gmail.com>
2017-05-11 20:19:39 +0100

    Completed definitions of the Workload tables - Moved workload points which require recalculation to workload_points_calculations

--- migrations/app/20170508150322_app_workload_report.js
+++ migrations/app/20170508150322_app_workload_report.js
@@ -2,9 +2,11 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('workload_report', function (table) {
     table.increments('id')
-    table.bigInteger('workload_points_id').unsigned().notNullable().references('workload_points.id')
     table.timestamp('effective_from').defaultTo(knex.fn.now())
     table.timestamp('effective_to')
+    table.integer('records_total').unsigned().notNullable()
+    table.string('status')
+    table.string('status_description')
   }).catch(function (error) {
     console.log(error)
     throw error
--- migrations/app/20170508150632_app_workload.js
+++ migrations/app/20170508150632_app_workload.js
@@ -3,7 +3,19 @@ exports.up = function (knex, Promise) {
   return knex.schema.withSchema('app').createTable('workload', function (table) {
     table.increments('id')
     table.integer('workload_owner_id').unsigned().references('workload_owner.id')
-    table.bigInteger('workload_report_id').unsigned().references('workload_report.id')
+    table.bigInteger('workload_points_calc_id').unsigned().references('workload_points_calculations.id')
+    table.integer('total_cases').unsigned().notNullable()
+    table.integer('total_cases_inactive').unsigned().notNullable()
+    table.integer('monthly_sdrs').unsigned().notNullable()
+    table.integer('sdr_due_next_30_days').unsigned().notNullable()
+    table.integer('active_warrants').unsigned().notNullable()
+    table.integer('overdue_terminations').unsigned().notNullable()
+    table.integer('unpaid_work').unsigned().notNullable()
+    table.integer('order_count').unsigned().notNullable()
+    table.integer('total_cases_ppo').unsigned()
+    table.integer('paroms_completed_last_30_days').unsigned().notNullable()
+    table.integer('paroms_due_next_30_days').unsigned().notNullable()
+    table.integer('lic_16_week_count').unsigned().notNullable()
   }).catch(function (error) {
     console.log(error)
     throw error
