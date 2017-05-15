26a8f17cba55b3e639027e4c0662cc187f354697
Andrew Wright <andrew.js.wright@gmail.com>
2017-05-15 14:19:49 +0100

    417: Add "Process Import" Task (#18)
    This is responsible for querying the `wmt_extract` table and creating tasks which
    specify the batches which need to be mapped by the `Create Import` task.
    
    Remaining work:
    - Add unit tests to verify correct tasks are being added to the database
    - Mark current task as complete
    - Add error handling

---
+++ app/services/domain/batch.js
@@ -0,0 +1,8 @@
+class Batch {
+  constructor (startingId, batchSize) {
+    this.startingId = startingId
+    this.batchSize = batchSize
+  }
+}
+
+module.exports = Batch
---
+++ test/unit/app/workers/test-process-import.js
@@ -0,0 +1,37 @@
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+const IdRange = require('../../../../app/services/domain/id-range')
+const expect = require('chai').expect
+
+var processImport
+var firstId = 1
+var lastId = 10000
+var createNewTasksStub
+var relativeFilePath = 'services/workers/process-import'
+var getWmtExtractStub
+
+describe(relativeFilePath, function () {
+  beforeEach(function () {
+    getWmtExtractStub = sinon.stub().returns(new IdRange(firstId, lastId))
+    createNewTasksStub = sinon.stub()
+    processImport = proxyquire('../../../../app/' + relativeFilePath, {
+      '../log': { info: function (message) { console.log(message) } },
+      '../data/get-wmt-extract-id-range': getWmtExtractStub,
+      '../data/create-tasks': createNewTasksStub
+    })
+  })
+
+  it('should call the database to get the ids', function (done) {
+    processImport.execute({}).then(function () {
+      expect(getWmtExtractStub.called).to.be.equal(true)
+      done()
+    })
+  })
+
+  it('should create 2,000 tasks given a batch size of 5', function (done) {
+    processImport.execute({}).then(function () {
+      expect(createNewTasksStub.getCall(0).args[0].length).to.equal(2000)
+      done()
+    })
+  })
+})
