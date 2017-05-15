69b0e088126139ea5eeb459ff3ec418bc1b8cab0
Tam Swann <ts.swann@gmail.com>
2017-05-15 11:24:49 +0100

    Bug/etl permissions (#21)
    * Fixing permissions of wmt ETL user
    
    * Corrected typos in migrations col names

--- database_setup/create-schemas-users.js
+++ database_setup/create-schemas-users.js
@@ -19,41 +19,45 @@ knex.schema
 
   // Create Roles:
     // app
-    .raw('CREATE ROLE appReadWrite;')
-    .raw('GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA app TO appReadWrite;')
+    .raw('CREATE ROLE appreadwrite;')
+    .raw('GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA app TO appreadwrite;')
 
     // app migration
-    .raw('CREATE ROLE appMigration;')
-    .raw('GRANT USAGE, CREATE ON SCHEMA app TO appMigration;')
+    .raw('CREATE ROLE appmigration;')
+    .raw('GRANT USAGE, CREATE ON SCHEMA app TO appmigration;')
 
     // staging
-    .raw('CREATE ROLE stagingReadWrite;')
-    .raw('GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA staging TO stagingReadWrite;')
+    .raw('CREATE ROLE stagingreadwrite;')
+    .raw('GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA staging TO stagingreadwrite;')
 
     // staging migration
-    .raw('CREATE ROLE stagingMigration;')
-    .raw('GRANT USAGE, CREATE ON SCHEMA staging TO stagingMigration;')
+    .raw('CREATE ROLE stagingmigration;')
+    .raw('GRANT USAGE, CREATE ON SCHEMA staging TO stagingmigration;')
 
   // Set User Permissions:
     // app
     .raw('ALTER USER ?? SET search_path TO app;', [config.WEB_APP_DATABASE_USERNAME])
-    .raw('GRANT appReadWrite TO ??;', [config.WEB_APP_DATABASE_USERNAME])
+    .raw('GRANT appreadwrite TO ??;', [config.WEB_APP_DATABASE_USERNAME])
 
     .raw('ALTER USER ?? SET search_path To app;', [config.MIGRATION_APP_DATABASE_USERNAME])
-    .raw('GRANT appMigration TO ??;', [config.MIGRATION_APP_DATABASE_USERNAME])
+    .raw('GRANT appmigration TO ??;', [config.MIGRATION_APP_DATABASE_USERNAME])
 
     .raw('ALTER USER ?? SET search_path To app;', [config.WORKER_DATABASE_USERNAME])
-    .raw('GRANT appReadWrite TO ??;', [config.WORKER_DATABASE_USERNAME])
+    .raw('GRANT appreadwrite TO ??;', [config.WORKER_DATABASE_USERNAME])
 
     // staging
     .raw('ALTER USER ?? SET search_path TO staging;', [config.ETL_STAGING_DATABASE_USERNAME])
-    .raw('GRANT stagingReadWrite TO ??;', [config.ETL_STAGING_DATABASE_USERNAME])
+    .raw('GRANT ALL ON SCHEMA staging TO ??;', [config.ETL_STAGING_DATABASE_USERNAME])
+    .raw('GRANT ALL PRIVILEGES ON SCHEMA staging TO ??;', [config.ETL_STAGING_DATABASE_USERNAME])
+    .raw('GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA staging TO ??;', [config.ETL_STAGING_DATABASE_USERNAME])
+    .raw('GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA staging TO ??', [config.ETL_STAGING_DATABASE_USERNAME])
+    .raw('GRANT SELECT ON ALL SEQUENCES IN SCHEMA staging TO ??', [config.ETL_STAGING_DATABASE_USERNAME])
 
     .raw('ALTER USER ?? SET search_path TO staging;', [config.MIGRATION_STG_DATABASE_USERNAME])
-    .raw('GRANT stagingMigration TO ??;', [config.MIGRATION_STG_DATABASE_USERNAME])
+    .raw('GRANT stagingmigration TO ??;', [config.MIGRATION_STG_DATABASE_USERNAME])
 
     .raw('ALTER USER ?? SET search_path TO staging;', [config.WORKER_DATABASE_USERNAME])
-    .raw('GRANT stagingReadWrite TO ??;', [config.WORKER_DATABASE_USERNAME])
+    .raw('GRANT stagingreadwrite TO ??;', [config.WORKER_DATABASE_USERNAME])
 
   .then(function () {
     process.exit(0)
--- migrations/staging/20170418155158_stg_inst_reports.js
+++ migrations/staging/20170418155158_stg_inst_reports.js
@@ -2,9 +2,14 @@
 exports.up = function (knex, Promise) {
   return knex.schema.withSchema('staging').createTable('inst_reports', function (table) {
     table.increments('id')
+    table.string('team_desc')
+    table.string('team_code')
+    table.string('om_name')
+    table.string('om_key')
     table.string('om_team_staff_grade')
     table.string('parom_due_next_30')
     table.string('parom_comp_last_30')
+    table.string('datestamp')
   }).catch(function (error) {
     console.log(error)
     throw error
