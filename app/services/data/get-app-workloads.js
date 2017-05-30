d663399bd04805a3e2103390dcc35808f68c7f42
Damien Coney <d.coney@kainos.com>
2017-05-30 09:57:11 +0100

    417: First iteration of SQL to get staging workload and the different Tiers. Next is to store the new workload points, workload points calculation and workload report.

---
+++ app/services/data/get-app-workloads.js
@@ -0,0 +1,43 @@
+const knexConfig = require('../../../knexfile').development
+const knex = require('knex')(knexConfig)
+const CaseDetails = require('wmt-probation-rules').CaseDetails
+const OmWorkload = require('wmt-probation-rules').OmWorkload
+
+module.exports = function (initialId, batchSize) {
+  var maxId = initialId + batchSize
+  var workloadReports = []
+
+
+  return knex.select().from('workload')
+    .join('community_tiers', 'workloadId', 'workload.id')
+    .join('licenser_tiers', 'workloadId', 'workload.id')
+    .join('custody_tiers', 'workloadId', 'workload.id')
+    .whereBetween('id', [initialId, maxId])
+    .then(function (results) {
+      var filteredResults = []
+      var casedetails = []
+      for (let i = initialId; i <= maxId; i++) {
+        filteredResults = results.filter(getByWorkloadId(i))
+        for (var result in filteredResults) {
+          casedetails.push(new CaseDetails(
+              result.row_type,
+              result.case_ref_no,
+              result.tier_code,
+              result.team_code,
+              result.om_grade_code,
+              result.om_key,
+              result.location,
+              result.workloadId
+            ))
+        }
+        workloadReports.push(new OmWorkload(undefined, undefined, undefined, casedetails, i))
+      }
+      return workloadReports
+    })
+}
+
+var getByWorkloadId = function (workloadId) {
+  return function (element) {
+    return element.workloadId === workloadId
+  }
+}
