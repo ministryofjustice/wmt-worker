c4246f1bebc616df648d48eba83ad890241aa905
Andrew Wright <a.wright@kainos.com>
2017-06-13 22:51:50 +0100

    417: Add inital unit test for calculate workload points

--- app/services/get-worker-for-task.js
+++ app/services/get-worker-for-task.js
@@ -2,7 +2,6 @@ var taskTypes = require('../constants/task-type')
 var calculateWorkloadPoints = require('./workers/calculate-workload-points')
 var processImport = require('./workers/process-import')
 var createWorkload = require('./workers/create-workload')
-var createWorkloadReport = require('./workers/create-workload-report')
 
 // ALL WORKERS SHOULD HAVE A METHOD `execute(task)` that returns a Promise
 module.exports = function (taskType) {
@@ -10,7 +9,6 @@ module.exports = function (taskType) {
     case taskTypes.CALCULATE_WORKLOAD_POINTS: return calculateWorkloadPoints
     case taskTypes.PROCESS_IMPORT: return processImport
     case taskTypes.CREATE_WORKLOAD: return createWorkload
-    case taskTypes.CREATE_WORKLOAD_REPORT: return createWorkloadReport
   }
 
   return null
--- test/unit/app/workers/test-calculate-workload-points.js
+++ test/unit/app/workers/test-calculate-workload-points.js
@@ -1,18 +1,75 @@
+const expect = require('chai').expect
 const proxyquire = require('proxyquire')
+const sinon = require('sinon')
 require('sinon-bluebird')
+const pointsHelper = require('wmt-probation-rules').pointsHelper
+
+const WORKLOAD_ID = 10
+const BATCH_SIZE = 20
+const REPORT_ID = 30
 
 var calculateWorkloadPoints
+var getWorkloadsStub
+var probationRulesStub
+var getPointsConfigurationStub
+var getOffenderManagerTypeIdStub
+var insertWorkloadPointsCalculationsStub
+var task
 
 describe('services/workers/calculate-workload-points', function () {
   beforeEach(function () {
+    getWorkloadsStub = sinon.stub()
+    getPointsConfigurationStub = sinon.stub()
+    getOffenderManagerTypeIdStub = sinon.stub()
+    insertWorkloadPointsCalculationsStub = sinon.stub()
+
+    probationRulesStub = {}
+    probationRulesStub.calculateWorkloadPoints = sinon.stub()
+    probationRulesStub.calculateTotalWorkloadPoints = sinon.stub()
+    probationRulesStub.calculateNominalTarget = sinon.stub()
+    probationRulesStub.calculateParomPoints = sinon.stub()
+    probationRulesStub.calculateSdrConversionPoints = sinon.stub()
+    probationRulesStub.calculateAvailablePoints = sinon.stub()
+
+    task = {id: 1, additionalData: {
+      workloadId: WORKLOAD_ID, batchSize: BATCH_SIZE, reportId: REPORT_ID
+    }}
+
     calculateWorkloadPoints = proxyquire('../../../../app/services/workers/calculate-workload-points', {
-      '../log': { info: function (message) {}, error: function (message) {} }
+      '../log': { info: function (message) {}, error: function (message) {} },
+      '../../services/data/get-app-workloads': getWorkloadsStub,
+      '../../services/data/get-workload-points-configuration': getPointsConfigurationStub,
+      '../../services/data/get-offender-manager-type-id': getOffenderManagerTypeIdStub,
+      '../../services/data/insert-workload-points-calculation': getOffenderManagerTypeIdStub,
+      'wmt-probation-rules': probationRulesStub
     })
   })
 
-  it('should call the calculate workload points worker', function () {
-    calculateWorkloadPoints.execute({id: 1, type: 'task'}).then(function () {
-      // TODO: add asserts
+  it('calls the get workloads promise correctly', function (done) {
+    getWorkloadsStub.resolves()
+    calculateWorkloadPoints.execute(task).then(function () {
+        expect(getWorkloadsStub.calledWith(WORKLOAD_ID, BATCH_SIZE)).to.equal(true)
+        done()
+    })
+  })
+
+  it('retrieves the points configuration', function (done) {
+    getWorkloadsStub.resolves()
+    calculateWorkloadPoints.execute(task).then(function () {
+      expect(getPointsConfigurationStub.called).to.equal(true)
+      done()
+    })
+  })
+
+  it('calls calculateSdrConversionPoints', function (done) {
+    insertWorkloadPointsCalculationsStub.resolves()
+    getOffenderManagerTypeIdStub.resolves(WORKLOAD_ID)
+    getPointsConfigurationStub.resolves({values: pointsHelper.getCaseTypeWeightings(), id: WORKLOAD_ID})
+    getWorkloadsStub.resolves([{values: sinon.stub(), id: WORKLOAD_ID}])
+
+    calculateWorkloadPoints.execute(task).then(function () {
+      expect(probationRulesStub.calculateSdrConversionPoints.called).to.equal(true)
+      done()
     })
   })
 })
