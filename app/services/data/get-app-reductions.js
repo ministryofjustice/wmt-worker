926c7bf001cf1fe60e9b3df3c8b8e1877b8d3c04
Steven Cochrane <s.cochrane@kainos.com>
2017-06-22 13:21:03 +0100

    WMT-562: fixed return of hours so that it returns an int

--- app/services/data/get-app-reductions.js
+++ app/services/data/get-app-reductions.js
@@ -3,9 +3,11 @@ const knex = require('knex')(knexConfig)
 
 module.exports = function (workloadOwnerId) {
   return knex('reductions').withSchema('app')
-    .sum('hours')
+    .sum('hours as hours')
     .where('workload_owner_id', workloadOwnerId)
     .where('effective_from', '<=', knex.fn.now())
     .andWhere('effective_to', '>=', knex.fn.now())
-    .then((result) => result)
+    .then(function (result) {
+      return result[0].hours
+    })
 }
