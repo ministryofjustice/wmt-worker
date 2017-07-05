2dd2e08bee336c6f1790b347de875b9b4fb90644
Damien Coney <d.coney@kainos.com>
2017-07-05 12:15:46 +0100

    424: adding views for region, team and ldu caseload capacity

--- migrations/app/20170620113855_region_capacity_view.js
+++ migrations/app/20170620113855_region_capacity_view.js
@@ -1,5 +1,5 @@
 exports.up = function (knex, Promise) {
-  var sql = `CREATE VIEW app.ldu_capacity_view 
+  var sql = `CREATE VIEW app.team_capacity_view 
       WITH SCHEMABINDING 
       AS 
       SELECT SUM(total_points) AS total_points
@@ -8,14 +8,13 @@ exports.up = function (knex, Promise) {
         , SUM(paroms_points) AS paroms_points
         , SUM(available_points) AS available_points
         , wr.effective_from AS effective_from
-        , ldu.id AS id
+        , t.id AS id
       FROM app.workload_points_calculations AS wpc
         JOIN app.workload AS w ON wpc.workload_id = w.id
         JOIN app.workload_owner AS wo ON w.workload_owner_id = wo.id
         JOIN app.team AS t ON wo.team_id = t.id
-        JOIN app.ldu AS ldu ON t.ldu_id = ldu.id
         JOIN app.workload_report AS wr ON wpc.workload_report_id = wr.id
-      GROUP BY ldu.id, wr.effective_from;`
+      GROUP BY t.id, wr.effective_from;`
 
   return knex.schema
            .raw('SET ARITHABORT ON')
@@ -24,5 +23,5 @@ exports.up = function (knex, Promise) {
 
 exports.down = function (knex, Promise) {
   return knex.schema
-      .raw('DROP VIEW app.ldu_capacity_view;')
+      .raw('DROP VIEW app.team_capacity_view;')
 }
