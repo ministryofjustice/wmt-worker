7d0e06d5873972064ff647af617da5f574990c39
Damien Coney <dconey646@users.noreply.github.com>
2017-07-07 12:07:41 +0100

    Feature/wmt 424/capacity views for region ldu team (#42)
    * 424: adding views for region, team and ldu caseload capacity
    
    * 424: Adding reduction hours to capacity views
    
    * 424: Add more seed data to allow for more data to show for the views

--- data/01000_workload_points_calculations.js
+++ data/01000_workload_points_calculations.js
@@ -1,15 +1,15 @@
 var tableName = 'workload_points_calculations'
 
 exports.seed = function (knex, Promise) {
-  var existingReportId
+  var existingReportIds
   var currentPointsId
     // Deletes ALL existing entries
   return knex(tableName).del()
     .then(function () {
-      return knex('workload_report').select('id').first()
+      return knex('workload_report').select('id')
     })
-    .then(function (reportId) {
-      existingReportId = reportId
+    .then(function (reportIds) {
+      existingReportIds = reportIds
       return knex('workload_points').select('id').first()
     })
     .then(function (workloadPointsId) {
@@ -20,11 +20,14 @@ exports.seed = function (knex, Promise) {
       var effectiveFromDate = new Date()
       effectiveFromDate.setDate(effectiveFromDate.getDate() - 365)
         // Inserts seed entries
-      return Promise.each(workloadIds, function (workloadId) {
-        effectiveFromDate.setDate(effectiveFromDate.getDate() + 30)
-        return knex(tableName).insert([
-              { workload_id: workloadId.id, workload_report_id: existingReportId.id, workload_points_id: currentPointsId.id, effective_from: effectiveFromDate, effective_to: effectiveFromDate.getDate() + 30, total_points: Math.floor(Math.random() * 25) + 30, available_points: 190, paroms_points: 50, sdr_conversion_points: 50, sdr_points: 50, nominal_target: 0, reduction_hours: Math.floor(Math.random() * 6) + 1 }
-        ])
-      })
+
+      var workloadPointsCalculationsToInsert = []
+      for( var i = 0; i < workloadIds.length; i++ ) {
+        var reportId = existingReportIds[i % existingReportIds.length]
+        var workloadId = workloadIds[i]
+        workloadPointsCalculationsToInsert.push({ workload_id: workloadId.id, workload_report_id: reportId.id, workload_points_id: currentPointsId.id, effective_from: effectiveFromDate, effective_to: effectiveFromDate.getDate() + 30, total_points: Math.floor(Math.random() * 25) + 30, available_points: 190, paroms_points: 50, sdr_conversion_points: 50, sdr_points: 50, nominal_target: 0, reduction_hours: Math.floor(Math.random() * 6) + 1 })
+        effectiveFromDate.setDate(effectiveFromDate.getDate() + 30)        
+      }
+      return knex(tableName).insert(workloadPointsCalculationsToInsert)
     })
 }
