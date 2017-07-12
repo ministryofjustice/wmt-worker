ad83a717b9a367bb931627fd3bd19751c8054e6d
Caoimhe Boyle <c.boyle@kainos.com>
2017-07-12 15:33:05 +0100

    334: Add seed data for tiers and more for workload
    Additional seed data to show functionality for case progress chart

--- data/00700_workload.js
+++ data/00700_workload.js
@@ -1,27 +1,19 @@
 var tableName = 'workload'
 
+const workloadRow = { workload_owner_id: 1, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 }
+
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex(tableName).del()
     .then(function () {
-      return knex('workload_owner').select('id').first()
+      return knex('workload_owner').select('id')
     })
-    .then(function (randomWorkloadOwnerId) {
-      // Inserts seed entries
-      return knex(tableName).insert([
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 },
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 },
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 },
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 },
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 },
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 },
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 },
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 },
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 },
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 },
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 },
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 },
-        { workload_owner_id: randomWorkloadOwnerId.id, total_cases: 15, monthly_sdrs: 10, sdr_due_next_30_days: 10, sdr_conversions_last_30_days: 10, total_community_cases: 5, total_custody_cases: 10, total_license_cases: 3, paroms_completed_last_30_days: 5, paroms_due_next_30_days: 5, license_last_16_weeks: 9, community_last_16_weeks: 10 }
-      ])
+    .then(function (workloadOwners) {
+      var workloadsToInsert = []
+      for (workloadOwner in workloadOwners) {
+        workloadsToInsert.push(Object.assign({}, workloadRow, {workload_owner_id: workloadOwners[workloadOwner].id})),
+        workloadsToInsert.push(Object.assign({}, workloadRow, {workload_owner_id: workloadOwners[workloadOwner].id}))          
+      }
+      return knex('workload').insert(workloadsToInsert)
     })
 }
