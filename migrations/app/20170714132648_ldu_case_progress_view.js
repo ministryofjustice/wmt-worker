0ae016a2999f2f49538511ce5d50b7c1f2714a92
Caoimhe Boyle <c.boyle@kainos.com>
2017-07-17 15:48:25 +0100

    636: Added create statement for ldu_case_progress_view

---
+++ migrations/app/20170714132648_ldu_case_progress_view.js
@@ -0,0 +1,26 @@
+exports.up = function(knex, Promise) {
+  var sql = `CREATE VIEW app.ldu_case_progress_view
+  WITH SCHEMABINDING
+  AS
+  SELECT 
+      SUM(tv.community_last_16_weeks) AS community_last_16_weeks
+    , SUM(tv.license_last_16_weeks) AS license_last_16_weeks
+    , SUM(tv.total_cases) AS total_cases
+    , SUM(tv.warrants_total) AS warrants_total
+    , SUM(tv.overdue_terminations_total) AS overdue_terminations_total
+    , SUM(tv.unpaid_work_total) AS unpaid_work_total
+    , MAX(l.id) AS id
+  FROM app.team_case_progress_view tv
+    JOIN app.team t ON t.id = tv.id
+    JOIN app.ldu l ON t.ldu_id = l.id
+  GROUP BY t.id;`
+
+  return knex.schema
+    .raw('SET ARITHABORT ON')
+    .raw(sql)  
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .raw('DROP VIEW app.ldu_case_progress_view;')
+}
