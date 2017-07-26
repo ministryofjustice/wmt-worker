7b1b8a279c0b08a12d659e68637c32233726d8c8
Andrew Wright <a.wright@kainos.com>
2017-07-26 15:41:45 +0100

    678: Remove unnecessary fields from capacity views
    Now that the worker is calaculating the total points we need to reflect that in
    the seed data to ensure the capacity graph is displayed correctly.

--- seed/data/01000_workload_points_calculations.js
+++ seed/data/01000_workload_points_calculations.js
@@ -27,7 +27,22 @@ exports.seed = function (knex, Promise) {
       for (var i = 0; i < workloadIds.length; i++) {
         var reportId = existingReportIds[i % existingReportIds.length]
         var workloadId = workloadIds[i]
-        workloadPointsCalculationsToInsert.push({ workload_id: workloadId.id, workload_report_id: reportId.id, workload_points_id: currentPointsId.id, effective_from: effectiveFromDate, effective_to: effectiveFromDate.getDate() + 30, total_points: Math.floor(Math.random() * 25) + 30, available_points: 190, paroms_points: 50, sdr_conversion_points: 50, sdr_points: 50, nominal_target: 0, reduction_hours: Math.floor(Math.random() * 6) + 1 })
+
+        workloadPointsCalculationsToInsert.push({
+          workload_id: workloadId.id,
+          workload_report_id: reportId.id,
+          workload_points_id: currentPointsId.id,
+          effective_from: effectiveFromDate,
+          effective_to: effectiveFromDate.getDate() + 30,
+          total_points: Math.floor(Math.random() * 25) + 180,
+          available_points: 190,
+          paroms_points: 50,
+          sdr_conversion_points: 50,
+          sdr_points: 50,
+          nominal_target: 0,
+          reduction_hours: Math.floor(Math.random() * 6) + 1
+        })
+
         effectiveFromDate.setDate(effectiveFromDate.getDate() + 30)
       }
       partOneWorkloadPointsCalculations = workloadPointsCalculationsToInsert.slice(0, workloadPointsCalculationsToInsert.length / 2)
