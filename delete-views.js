217cb36f6bfa4694cedae1cbeffd7a819c639174
Andrew Wright <andrew.js.wright@gmail.com>
2017-07-27 10:43:58 +0100

    678: Add script to delete views (#58)
    * 678: Add script to delete views
    
    This needs to be run before a rollback so the base tables can be deleted.
    
    Run using: `node delete-views.js`
    
    * Renamed variables

---
+++ delete-views.js
@@ -0,0 +1,14 @@
+const config = require('./knexfile').app
+const knex = require('knex')(config)
+const glob = require('glob')
+const Promise = require('bluebird').Promise
+
+var seedViewFileNames = glob.sync('./seed/views/[0...9]*.js')
+
+var databaseViewNames = seedViewFileNames.sort().reverse()
+    .map((fileName) => fileName.substring(fileName.lastIndexOf('/') + 7, fileName.lastIndexOf('.')))
+
+Promise.each(databaseViewNames, (viewName) =>
+        knex.schema.raw('DROP VIEW IF EXISTS ' + viewName).return()
+)
+.finally(() => knex.destroy())
