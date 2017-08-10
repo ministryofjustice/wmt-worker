f5ce8c28651f28c8d068ad9ff93076f69c386ce7
tsheppard01 <t.sheppard@kainos.com>
2017-08-10 12:37:31 +0100

    Hotfix: contracted_hours test fixes

--- seed/data/00600_workload_owner.js
+++ seed/data/00600_workload_owner.js
@@ -12,17 +12,17 @@ exports.seed = function (knex, Promise) {
     })
     .then(function (omIds) {
       // Inserts seed entries
-      var contracted_hours = [37.5,36.5,35.5,33.5,32.5]
+      var contractedHours = [37.5, 36.5, 35.5, 33.5, 32.5]
       var insertData = []
 
       var counter = 0
-      omIds.forEach(function(omId) {
-        var hours = contracted_hours[counter++]
+      omIds.forEach(function (omId) {
+        var hours = contractedHours[counter++]
         insertData.push(
-          { offender_manager_id: omId.id, team_id: teamIds[0].id, contracted_hours: hours },
-          { offender_manager_id: omId.id, team_id: teamIds[1].id, contracted_hours: hours },
-          { offender_manager_id: omId.id, team_id: teamIds[2].id, contracted_hours: hours },
-          { offender_manager_id: omId.id, team_id: teamIds[3].id, contracted_hours: hours }
+          { offender_manager_id: omId.id, team_id: teamIds[0].id, contractedHours: hours },
+          { offender_manager_id: omId.id, team_id: teamIds[1].id, contractedHours: hours },
+          { offender_manager_id: omId.id, team_id: teamIds[2].id, contractedHours: hours },
+          { offender_manager_id: omId.id, team_id: teamIds[3].id, contractedHours: hours }
         )
       })
       return knex(tableName).insert(insertData)
