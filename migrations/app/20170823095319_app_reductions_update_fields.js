ead3bd0555f164710b1419ada97e892991cabbeb
Caoimhe Boyle <c.boyle@kainos.com>
2017-08-23 11:30:34 +0100

    823: Fix standard errors

--- migrations/app/20170823095319_app_reductions_update_fields.js
+++ migrations/app/20170823095319_app_reductions_update_fields.js
@@ -1,15 +1,17 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.alterTable('reductions', function(table) {
+exports.up = function (knex, Promise) {
+  return knex.schema.alterTable('reductions', function (table) {
     // Add 'reduction_reason_id' and 'notes' fields
     table.integer('reduction_reason_id').unsigned().notNullable().references('reduction_reason.id')
     table.string('notes')
     // Drop notNullable constraint from 'effective_to'
-    table.timestamp('effective_to').alter() 
-  });  
-};
+    table.timestamp('effective_to').alter()
+  })
+}
 
-exports.down = function(knex, Promise) {
-  table.dropColumn('reduction_reason_id')
-  table.dropColumn('notes')
-  table.timestamp('effective_to').notNullable().alter() 
-};
+exports.down = function (knex, Promise) {
+  return knex.schema.alterTable('reductions', function (table) {
+    table.dropColumn('reduction_reason_id')
+    table.dropColumn('notes')
+    table.timestamp('effective_to').notNullable().alter()
+  })
+}
