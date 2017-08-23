887c4190ec2dcc8caf5b1e0cf13035db7f416cc2
Damien Coney <dconey646@users.noreply.github.com>
2017-08-23 15:10:13 +0100

    Revert "Feature/wmt 823/reductions"

--- delete-data.js
+++ delete-data.js
@@ -3,8 +3,7 @@ const knex = require('knex')(config)
 const glob = require('glob')
 const Promise = require('bluebird').Promise
 
-var dataDirectory = process.argv[2]
-var seedDataFileNames = glob.sync('./seed/data/' + dataDirectory + '/[0...9]*.js')
+var seedDataFileNames = glob.sync('./seed/data/[0...9]*.js')
 
 var databaseTableNames = seedDataFileNames.sort().reverse()
     .map((fileName) => fileName.substring(fileName.lastIndexOf('/') + 7, fileName.lastIndexOf('.')))
