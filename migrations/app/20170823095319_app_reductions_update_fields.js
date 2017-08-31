ecc2ec1be60f7a1a899f78e33ed20b0cbfbc8df6
Caoimhe Boyle <c.boyle@kainos.com>
2017-08-31 13:37:23 +0100

    843: Update migrations and seed data for reduction ref data

--- migrations/app/20170823095319_app_reductions_update_fields.js
+++ migrations/app/20170823095319_app_reductions_update_fields.js
@@ -1,7 +1,8 @@
 exports.up = function (knex, Promise) {
   return knex.schema.alterTable('reductions', function (table) {
     // Add 'reduction_reason_id' and 'notes' fields
-    table.integer('reduction_reason_id').unsigned().notNullable().references('reduction_reason.id')
+    // Default reduction_reason_id to ID of "Other" in the reduction_reason table
+    table.integer('reduction_reason_id').unsigned().defaultTo(11).notNullable().references('reduction_reason.id')
     table.string('notes')
     // Drop notNullable constraint from 'effective_to'
     table.timestamp('effective_to').alter()
--- seed/data/ref/00200_reduction_reason.js
+++ seed/data/ref/00200_reduction_reason.js
@@ -1,5 +1,7 @@
 var tableName = 'reduction_reason'
+var insertStatement = 'INSERT INTO app.' + tableName + ' (id, reason, reason_short_name, category_id, allowance_percentage, max_allowance_percentage, months_to_expiry) VALUES '
 var categoryId
+var sql
 
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
@@ -10,42 +12,45 @@ exports.seed = function (knex, Promise) {
     .then(function (categoryIdResult) {
       categoryId = categoryIdResult.id
       // Inserts seed entries
-      return knex(tableName).insert([
-        { reason: 'Disability', reason_short_name: 'Disability', category_id: categoryId, allowance_percentage: undefined, max_allowance_percentage: undefined, months_to_expiry: undefined },
-        { reason: 'Long Term Sickness Absence', reason_short_name: 'Long Term Sickness Absence', category_id: categoryId, allowance_percentage: 100, max_allowance_percentage: undefined, months_to_expiry: undefined },
-        { reason: 'Phased Return to Work', reason_short_name: 'Phased Return to Work', category_id: categoryId, allowance_percentage: undefined, max_allowance_percentage: undefined, months_to_expiry: undefined },
-        { reason: 'Pregnancy', reason_short_name: 'Pregnancy', category_id: categoryId, allowance_percentage: undefined, max_allowance_percentage: undefined, months_to_expiry: undefined },
-        { reason: 'Maternity Leave', reason_short_name: 'Maternity Leave', category_id: categoryId, allowance_percentage: 100, max_allowance_percentage: undefined, months_to_expiry: 6 },
-        { reason: 'Adoption Leave', reason_short_name: 'Adoption Leave', category_id: categoryId, allowance_percentage: 100, max_allowance_percentage: undefined, months_to_expiry: 6 },
-        { reason: 'Special Leave', reason_short_name: 'Special Leave', category_id: categoryId, allowance_percentage: 100, max_allowance_percentage: undefined, months_to_expiry: undefined },
-        { reason: 'Trade Union Facility Time', reason_short_name: 'Trade Union Facility Time', category_id: categoryId, allowance_percentage: undefined, max_allowance_percentage: 50, months_to_expiry: undefined },
-        { reason: 'Other Paid Leave (e.g. Jury Service)', reason_short_name: 'Other Paid Leave (e.g. Jury Service)', category_id: categoryId, allowance_percentage: 100, max_allowance_percentage: undefined, months_to_expiry: undefined },
-        { reason: 'Other Unpaid Leave', reason_short_name: 'Other Unpaid Leave', category_id: categoryId, allowance_percentage: 100, max_allowance_percentage: undefined, months_to_expiry: undefined },
-        { reason: 'Other', reason_short_name: 'Other', category_id: categoryId, allowance_percentage: undefined, max_allowance_percentage: undefined, months_to_expiry: undefined }
-      ])
+      sql = 'SET IDENTITY_INSERT app.' + tableName + ' ON;' +
+      insertStatement + '(1, \'Disability\', \'Disability\', ' + categoryId +', null, null, null)' +
+      insertStatement + '(2, \'Long Term Sickness Absence\', \'Long Term Sickness Absence\', ' + categoryId + ', 100, null, null)' +
+      insertStatement + '(3, \'Phased Return to Work\', \'Phased Return to Work\', ' + categoryId +' , null, null, null)' +
+      insertStatement + '(4, \'Pregnancy\', \'Pregnancy\', ' + categoryId + ', null, null, null)' +
+      insertStatement + '(5, \'Maternity Leave\', \'Maternity Leave\', ' + categoryId + ', 100, null, 6)' +
+      insertStatement + '(6, \'Adoption Leave\', \'Adoption Leave\', ' + categoryId + ', 100, null, 6)' +
+      insertStatement + '(7, \'Special Leave\', \'Special Leave\', ' + categoryId + ', 100, null, null)' +
+      insertStatement + '(8, \'Trade Union Facility Time\', \'Trade Union Facility Time\', ' + categoryId +', null, 50, null)' +
+      insertStatement + '(9, \'Other Paid Leave (e.g. Jury Service)\', \'Other Paid Leave (e.g. Jury Service)\', ' + categoryId +', 100, null, null)' +
+      insertStatement + '(10, \'Other Unpaid Leave\', \'Other Unpaid Leave\', ' + categoryId +', 100, null, null)' +
+      insertStatement + '(11, \'Other\', \'Other\', ' + categoryId +', null, null, null)'
+      return knex.schema
+        .raw(sql)
     })
     .then(function () {
       return knex('reduction_category').select('id').where({category: 'Community Justice Learning'}).first()
     })
     .then(function (categoryIdResult) {
       categoryId = categoryIdResult.id
-      return knex(tableName).insert([
-        { reason: 'Probation Qualification Framework/Professional Qualification in Probation - 1st 6 months', reason_short_name: 'PQiP - 1st 6 months', category_id: categoryId, allowance_percentage: 80, max_allowance_percentage: undefined, months_to_expiry: 6 },
-        { reason: 'Probation Qualification Framework/Professional Qualification in Probation - 6 to 12 months', reason_short_name: 'PQiP - 6 to 12 months', category_id: categoryId, allowance_percentage: 60, max_allowance_percentage: undefined, months_to_expiry: 6 },
-        { reason: 'Probation Qualification Framework/Professional Qualification in Probation - 12 to 18 months', reason_short_name: 'PQiP - 12 to 18 months', category_id: categoryId, allowance_percentage: 40, max_allowance_percentage: undefined, months_to_expiry: 6 },
-        { reason: 'Newly Qualified Probation Officers', reason_short_name: 'NQO', category_id: categoryId, allowance_percentage: 20, max_allowance_percentage: undefined, months_to_expiry: 9 },
-        { reason: 'PSO Learning & Development', reason_short_name: 'PSO Learning & Development', category_id: categoryId, allowance_percentage: 20, max_allowance_percentage: undefined, months_to_expiry: 6 },
-        { reason: 'Vocational Qualification Level 3 (VQ3)', reason_short_name: 'VQ3', category_id: categoryId, allowance_percentage: 5, max_allowance_percentage: undefined, months_to_expiry: 6 },
-        { reason: 'Level 4 and Level 5 Access', reason_short_name: 'Level 4 or 5 Access', category_id: categoryId, allowance_percentage: 10, max_allowance_percentage: undefined, months_to_expiry: 6 }
-      ])
+      sql = 'SET IDENTITY_INSERT app.' + tableName + ' ON;' +
+      insertStatement + '(12, \'Probation Qualification Framework/Professional Qualification in Probation - 1st 6 months\', \'PQiP - 1st 6 months\', ' + categoryId +', 80, null, 6)' +
+      insertStatement + '(13, \'Probation Qualification Framework/Professional Qualification in Probation - 6 to 12 months\', \'PQiP - 6 to 12 months\', ' + categoryId +', 60, null, 6)' +
+      insertStatement + '(14, \'Probation Qualification Framework/Professional Qualification in Probation - 12 to 18 months\', \'PQiP - 12 to 18 months\', ' + categoryId +', 40, null, 6)' +
+      insertStatement + '(15, \'Newly Qualified Probation Officers\', \'NQO\', ' + categoryId +', 20, null, 9)' +
+      insertStatement + '(16, \'PSO Learning & Development\', \'PSO Learning & Development\', ' + categoryId +', 20, null, 6)' +
+      insertStatement + '(17, \'Vocational Qualification Level 3 (VQ3)\', \'VQ3\', ' + categoryId +', 5, null, 6)' +
+      insertStatement + '(18, \'Level 4 and Level 5 Access\', \'Level 4 and Level 5 Access\', ' + categoryId +', 10, null, 6)'
+      return knex.schema
+        .raw(sql)
     })
     .then(function () {
       return knex('reduction_category').select('id').where({category: 'Work Circumstances'}).first()
     })
     .then(function (categoryIdResult) {
       categoryId = categoryIdResult.id
-      return knex(tableName).insert([
-        { reason: 'Co-Worked Cases (not CMS)', reason_short_name: 'Co-Worked Cases', category_id: categoryId, allowance_percentage: undefined, max_allowance_percentage: undefined, months_to_expiry: undefined }
-      ])
+      sql = 'SET IDENTITY_INSERT app.' + tableName + ' ON;' +
+      insertStatement + '(19, \'Co-Worked Cases (not CMS)\', \'Co-Worked Cases (not CMS)\', ' + categoryId +', null, null, null)'
+      return knex.schema
+        .raw(sql)
     })
 }
