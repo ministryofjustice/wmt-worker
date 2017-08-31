d9ad9b19cdfc837c16074e2ba443e0104eaee218
Caoimhe Boyle <c.boyle@kainos.com>
2017-08-31 14:03:34 +0100

    843: Readd commented out code

--- seed/data/ref/00100_reduction_category.js
+++ seed/data/ref/00100_reduction_category.js
@@ -3,13 +3,13 @@ var insertStatement = 'INSERT INTO app.' + tableName + ' (id, category) VALUES '
 
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  // return knex(tableName).del()
-  //   .then(function () {
-  var sql = 'SET IDENTITY_INSERT app.' + tableName + ' ON;' +
+  return knex(tableName).del()
+    .then(function () {
+      var sql = 'SET IDENTITY_INSERT app.' + tableName + ' ON;' +
           insertStatement + '(1, \'Personal Circumstances\')' +
           insertStatement + '(2, \'Community Justice Learning\')' +
           insertStatement + '(3, \'Work Circumstances\')'
-  return knex.schema
+      return knex.schema
         .raw(sql)
-    // })
+    })
 }
