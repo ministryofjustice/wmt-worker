610c30646740274c6f753940d8207929abfa7391
tsheppard01 <t.sheppard@kainos.com>
2017-09-13 13:48:05 +0100

    989: Adds cms staging table

---
+++ migrations/staging/20170913133838_stg_cms.js
@@ -0,0 +1,28 @@
+
+exports.up = function (knex, Promise) {
+    return knex.schema.withSchema('staging').createTable('cms', function (table) {
+      table.increments('id')
+      table.string('contact_id')
+      table.string('contact_date')
+      table.string('contact_type_code')
+      table.string('contact_type_desc')
+      table.string('contact_staff_name')
+      table.string('contact_staff_key')
+      table.string('contact_staff_grade')
+      table.string('contact_team_key')
+      table.string('contact_provider_code')
+      table.string('om_name')
+      table.string('om_key')
+      table.string('om_grade')
+      table.string('om_team_key')
+      table.string('om_provider_code')
+    }).catch(function (error) {
+      console.log(error)
+      throw error
+    })
+  }
+  
+  exports.down = function (knex, Promise) {
+    knex.schema.withSchema('staging').dropTable('cms')
+  }
+  
