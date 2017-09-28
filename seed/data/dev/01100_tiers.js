10fd340c37b18e8fcd0f7a6bd9714463691d2884
tsheppard01 <t.sheppard@kainos.com>
2017-09-28 17:02:43 +0100

    1091: Changes tiers seed data so it is different for each location

--- seed/data/dev/01100_tiers.js
+++ seed/data/dev/01100_tiers.js
@@ -2,24 +2,41 @@ var tableName = 'tiers'
 var insertData = []
 
 exports.seed = function (knex, Promise) {
-  var locations = ['COMMUNITY', 'CUSTODY', 'LICENSE']
   // Deletes ALL existing entries
   return knex(tableName).del()
     .return(knex('workload').select('id'))
     .then(function (workloads) {
       workloads.forEach(function (workload) {
-        locations.forEach(function (location) {
-          insertData.push(
-            { workload_id: workload.id, tier_number: 0, overdue_terminations_total: 0, unpaid_work_total: 0, warrants_total: 0, total_cases: 0, location: location },
-            { workload_id: workload.id, tier_number: 1, overdue_terminations_total: 1, unpaid_work_total: 0, warrants_total: 0, total_cases: 1, location: location },
-            { workload_id: workload.id, tier_number: 2, overdue_terminations_total: 1, unpaid_work_total: 1, warrants_total: 0, total_cases: 2, location: location },
-            { workload_id: workload.id, tier_number: 3, overdue_terminations_total: 1, unpaid_work_total: 1, warrants_total: 1, total_cases: 3, location: location },
-            { workload_id: workload.id, tier_number: 4, overdue_terminations_total: 2, unpaid_work_total: 0, warrants_total: 0, total_cases: 2, location: location },
-            { workload_id: workload.id, tier_number: 5, overdue_terminations_total: 2, unpaid_work_total: 2, warrants_total: 0, total_cases: 4, location: location },
-            { workload_id: workload.id, tier_number: 6, overdue_terminations_total: 2, unpaid_work_total: 2, warrants_total: 2, total_cases: 6, location: location },
-            { workload_id: workload.id, tier_number: 7, overdue_terminations_total: 2, unpaid_work_total: 1, warrants_total: 0, total_cases: 3, location: location }
-          )
-        })
+        insertData.push(
+          { workload_id: workload.id, tier_number: 0, overdue_terminations_total: 0, unpaid_work_total: 0, warrants_total: 0, total_cases: 0, location: 'COMMUNITY' },
+          { workload_id: workload.id, tier_number: 1, overdue_terminations_total: 1, unpaid_work_total: 0, warrants_total: 0, total_cases: 1, location: 'COMMUNITY' },
+          { workload_id: workload.id, tier_number: 2, overdue_terminations_total: 1, unpaid_work_total: 1, warrants_total: 0, total_cases: 2, location: 'COMMUNITY' },
+          { workload_id: workload.id, tier_number: 3, overdue_terminations_total: 1, unpaid_work_total: 1, warrants_total: 1, total_cases: 3, location: 'COMMUNITY' },
+          { workload_id: workload.id, tier_number: 4, overdue_terminations_total: 2, unpaid_work_total: 0, warrants_total: 0, total_cases: 2, location: 'COMMUNITY' },
+          { workload_id: workload.id, tier_number: 5, overdue_terminations_total: 2, unpaid_work_total: 2, warrants_total: 0, total_cases: 4, location: 'COMMUNITY' },
+          { workload_id: workload.id, tier_number: 6, overdue_terminations_total: 2, unpaid_work_total: 2, warrants_total: 2, total_cases: 6, location: 'COMMUNITY' },
+          { workload_id: workload.id, tier_number: 7, overdue_terminations_total: 2, unpaid_work_total: 1, warrants_total: 0, total_cases: 3, location: 'COMMUNITY' }
+        )
+        insertData.push(
+          { workload_id: workload.id, tier_number: 1, overdue_terminations_total: 0, unpaid_work_total: 0, warrants_total: 0, total_cases: 0, location: 'CUSTODY' },
+          { workload_id: workload.id, tier_number: 0, overdue_terminations_total: 1, unpaid_work_total: 0, warrants_total: 0, total_cases: 1, location: 'CUSTODY' },
+          { workload_id: workload.id, tier_number: 3, overdue_terminations_total: 1, unpaid_work_total: 1, warrants_total: 0, total_cases: 2, location: 'CUSTODY' },
+          { workload_id: workload.id, tier_number: 2, overdue_terminations_total: 1, unpaid_work_total: 1, warrants_total: 1, total_cases: 3, location: 'CUSTODY' },
+          { workload_id: workload.id, tier_number: 5, overdue_terminations_total: 2, unpaid_work_total: 0, warrants_total: 0, total_cases: 2, location: 'CUSTODY' },
+          { workload_id: workload.id, tier_number: 4, overdue_terminations_total: 2, unpaid_work_total: 2, warrants_total: 0, total_cases: 4, location: 'CUSTODY' },
+          { workload_id: workload.id, tier_number: 7, overdue_terminations_total: 2, unpaid_work_total: 2, warrants_total: 2, total_cases: 6, location: 'CUSTODY' },
+          { workload_id: workload.id, tier_number: 6, overdue_terminations_total: 2, unpaid_work_total: 1, warrants_total: 0, total_cases: 3, location: 'CUSTODY' }
+        )
+        insertData.push(
+          { workload_id: workload.id, tier_number: 7, overdue_terminations_total: 0, unpaid_work_total: 0, warrants_total: 0, total_cases: 0, location: 'LICENSE' },
+          { workload_id: workload.id, tier_number: 6, overdue_terminations_total: 1, unpaid_work_total: 0, warrants_total: 0, total_cases: 1, location: 'LICENSE' },
+          { workload_id: workload.id, tier_number: 5, overdue_terminations_total: 1, unpaid_work_total: 1, warrants_total: 0, total_cases: 2, location: 'LICENSE' },
+          { workload_id: workload.id, tier_number: 4, overdue_terminations_total: 1, unpaid_work_total: 1, warrants_total: 1, total_cases: 3, location: 'LICENSE' },
+          { workload_id: workload.id, tier_number: 3, overdue_terminations_total: 2, unpaid_work_total: 0, warrants_total: 0, total_cases: 2, location: 'LICENSE' },
+          { workload_id: workload.id, tier_number: 2, overdue_terminations_total: 2, unpaid_work_total: 2, warrants_total: 0, total_cases: 4, location: 'LICENSE' },
+          { workload_id: workload.id, tier_number: 1, overdue_terminations_total: 2, unpaid_work_total: 2, warrants_total: 2, total_cases: 6, location: 'LICENSE' },
+          { workload_id: workload.id, tier_number: 0, overdue_terminations_total: 2, unpaid_work_total: 1, warrants_total: 0, total_cases: 3, location: 'LICENSE' }
+        )
       })
 
       // Need to split the array into 16 as one query caused an error with too many parameters in one request (2100)
