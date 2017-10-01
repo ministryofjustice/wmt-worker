841b51de4dbfba04770082a5f30bb9ce4753be07
Tom Swann <ts.swann@gmail.com>
2017-10-01 16:45:28 +0100

    Fixed bug with regular reductions and CMS adjustments

--- app/services/data/get-adjustments.js
+++ app/services/data/get-adjustments.js
@@ -1,16 +1,16 @@
 const knexConfig = require('../../../knexfile').app
 const knex = require('knex')(knexConfig)
 
-const reductionStatus = require('../../constants/reduction-status')
+const adjustmentStatus = require('../../constants/adjustment-status')
 
 module.exports = function (category) {
   return knex('adjustments')
     .join('adjustment_reason', 'adjustment_reason.id', 'adjustments.adjustment_reason_id')
     .whereNotNull('contact_id')
-    .andWhere('status', reductionStatus.ACTIVE)
+    .andWhere('status', adjustmentStatus.ACTIVE)
     .andWhere('adjustment_reason.category_id', category)
-    .select('id',
+    .select('adjustments.id',
       'workload_owner_id AS workloadOwnerId',
       'contact_id AS contactId',
-      'points AS points')
+      'adjustments.points AS points')
 }
