6a175c42f278acb38ebb55bba890fdda9489b344
Tom Swann <ts.swann@gmail.com>
2017-10-01 12:30:03 +0100

    Refactor CMS data modules to retrieve from adjustments

--- app/services/data/get-adjustment-points.js
+++ app/services/data/get-adjustment-points.js
@@ -2,17 +2,19 @@ const knexConfig = require('../../../knexfile').app
 const knex = require('knex')(knexConfig)
 const reductionStatus = require('../../constants/reduction-status')
 
-module.exports = function (workloadOwnerId) {
-  return knex('reductions')
-    .sum('hours AS hours')
+module.exports = function (workloadOwnerId, category) {
+  return knex('adjustments')
+    .join('adjustment_reason', 'adjustment_reason.id', 'adjustments.adjustment_reason_id')
+    .sum('points AS points')
     .where('workload_owner_id', workloadOwnerId)
     .whereNotNull('contact_id')
     .andWhere('status', reductionStatus.ACTIVE)
+    .andWhere('adjustment_reason.category_id', category)
     .then(function (result) {
-      if (result === null || result[0].hours === null) {
+      if (result === null || result[0].points === null) {
         return 0
       } else {
-        return result[0].hours
+        return result[0].points
       }
     })
 }
--- app/services/data/get-adjustments.js
+++ app/services/data/get-adjustments.js
@@ -3,12 +3,14 @@ const knex = require('knex')(knexConfig)
 
 const reductionStatus = require('../../constants/reduction-status')
 
-module.exports = function () {
-  return knex('reductions')
+module.exports = function (category) {
+  return knex('adjustments')
+    .join('adjustment_reason', 'adjustment_reason.id', 'adjustments.adjustment_reason_id')
     .whereNotNull('contact_id')
     .andWhere('status', reductionStatus.ACTIVE)
+    .andWhere('adjustment_reason.category_id', category)
     .select('id',
       'workload_owner_id AS workloadOwnerId',
       'contact_id AS contactId',
-      'hours AS hours')
+      'points AS points')
 }
