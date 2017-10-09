3fafef243629eebf7275482e1cc3efb509e0b3fc
tsheppard01 <t.sheppard@kainos.com>
2017-10-09 12:37:37 +0100

    175: Adds oral_reports to court_reports table in staging

---
+++ migrations/staging/20171009120412_stg_add_oral_reports_to_court_reports.js
@@ -0,0 +1,16 @@
+
+exports.up = function (knex, Promise) {
+    return knex.schema.withSchema('staging').table('court_reports', function (table) {
+      table.string('oral_reports')
+    }).catch(function (error) {
+      console.log(error)
+      throw error
+    })
+  }
+  
+  exports.down = function (knex, Promise) {
+    return knex.schema.withSchema('staging').table('arms', function (table) {
+        table.dropColumn('oral_reports')
+    })
+  }
+  
