39596f9d3438779c7b4a1508f27f11fefcaaa110
tsheppard01 <t.sheppard@kainos.com>
2017-10-09 14:26:54 +0100

    175: Adds is_court_reporter to workload_owner table

--- migrations/app/20171009124100_app_court_report_workload.js
+++ migrations/app/20171009124100_app_court_report_workload.js
@@ -13,6 +13,6 @@ exports.up = function (knex, Promise) {
   }
   
   exports.down = function (knex, Promise) {
-    return knex.schema.dropTable('court_reports_workload')
+    return knex.schema.dropTable('court_report_workload')
   }
   
