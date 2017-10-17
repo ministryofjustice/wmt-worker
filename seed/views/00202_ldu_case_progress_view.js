56cd4962673eba66aac0ceb2325173c96fa00f7c
Andrew Wright <a.wright@kainos.com>
2017-10-17 19:38:43 +0100

    Add indexing to Case Progress Views

--- seed/views/00202_ldu_case_progress_view.js
+++ seed/views/00202_ldu_case_progress_view.js
@@ -1,23 +1,32 @@
 exports.seed = function (knex, Promise) {
-  var sql = `CREATE VIEW app.ldu_case_progress_view
+  var view = `CREATE VIEW app.ldu_case_progress_view
   WITH SCHEMABINDING
   AS
   SELECT
-      MAX(t.description) AS name 
-    , SUM(tv.community_last_16_weeks) AS community_last_16_weeks
-    , SUM(tv.license_last_16_weeks) AS license_last_16_weeks
-    , SUM(tv.total_cases) AS total_cases
-    , SUM(tv.warrants_total) AS warrants_total
-    , SUM(tv.overdue_terminations_total) AS overdue_terminations_total
-    , SUM(tv.unpaid_work_total) AS unpaid_work_total
-    , MAX(l.id) AS id
-  FROM app.team_case_progress_view tv
-    JOIN app.team t ON t.id = tv.id
+      t.description AS name
+    , SUM(w.community_last_16_weeks) AS community_last_16_weeks
+    , SUM(w.license_last_16_weeks) AS license_last_16_weeks
+    , SUM(w.total_cases) AS total_cases
+    , SUM(tr.warrants_total) AS warrants_total
+    , SUM(tr.overdue_terminations_total) AS overdue_terminations_total
+    , SUM(tr.unpaid_work_total) AS unpaid_work_total
+    , l.id AS id
+    , COUNT_BIG(*) AS count
+  FROM app.workload w
+    JOIN app.workload_points_calculations wpc ON wpc.workload_id = w.id
+    JOIN app.workload_report wr ON wr.id = wpc.workload_report_id
+    JOIN app.tiers tr ON tr.workload_id = w.id
+    JOIN app.workload_owner wo ON wo.id = w.workload_owner_id
+    JOIN app.team t ON wo.team_id = t.id
     JOIN app.ldu l ON t.ldu_id = l.id
-  GROUP BY t.id;`
+  GROUP BY t.description, l.id;`
+
+  var index = `CREATE UNIQUE CLUSTERED INDEX idx_ldu_case_progress_view
+  ON app.ldu_case_progress_view (name)`
 
   return knex.schema
     .raw('DROP VIEW IF EXISTS app.ldu_case_progress_view;')
     .raw('SET ARITHABORT ON')
-    .raw(sql)
+    .raw(view)
+    .raw(index)
 }
--- seed/views/00203_region_case_progress_view.js
+++ seed/views/00203_region_case_progress_view.js
@@ -1,23 +1,35 @@
 exports.seed = function (knex, Promise) {
-  var sql = `CREATE VIEW app.region_case_progress_view
+  var view = `CREATE VIEW app.region_case_progress_view
   WITH SCHEMABINDING
   AS
   SELECT 
-      MAX(l.description) AS name
-    , SUM(lv.community_last_16_weeks) AS community_last_16_weeks
-    , SUM(lv.license_last_16_weeks) AS license_last_16_weeks
-    , SUM(lv.total_cases) AS total_cases
-    , SUM(lv.warrants_total) AS warrants_total
-    , SUM(lv.overdue_terminations_total) AS overdue_terminations_total
-    , SUM(lv.unpaid_work_total) AS unpaid_work_total
-    , MAX(r.id) AS id
-  FROM app.ldu_case_progress_view lv
-    JOIN app.ldu l ON l.id = lv.id
+      l.description AS name
+    , SUM(w.community_last_16_weeks) AS community_last_16_weeks
+    , SUM(w.license_last_16_weeks) AS license_last_16_weeks
+    , SUM(w.total_cases) AS total_cases
+    , SUM(tr.warrants_total) AS warrants_total
+    , SUM(tr.overdue_terminations_total) AS overdue_terminations_total
+    , SUM(tr.unpaid_work_total) AS unpaid_work_total
+    , r.id AS id
+    , COUNT_BIG(*) AS count
+  FROM app.workload w
+    JOIN app.workload_points_calculations wpc ON wpc.workload_id = w.id
+    JOIN app.workload_report wr ON wr.id = wpc.workload_report_id
+    JOIN app.tiers tr ON tr.workload_id = w.id
+    JOIN app.workload_owner wo ON wo.id = w.workload_owner_id
+    JOIN app.team t ON wo.team_id = t.id
+    JOIN app.ldu l ON l.id = t.ldu_id
     JOIN app.region r ON r.id = l.region_id
-  GROUP BY l.id;`
+  GROUP BY 
+    r.id
+  , l.description;`
+
+  var index = `CREATE UNIQUE CLUSTERED INDEX idx_region_case_progress_view
+  ON app.region_case_progress_view (name)`
 
   return knex.schema
     .raw('DROP VIEW IF EXISTS app.region_case_progress_view;')
     .raw('SET ARITHABORT ON')
-    .raw(sql)
+    .raw(view)
+    .raw(index)
 }
