234196414ce7e44c1f38fae30a508ce76696e4e8
tsheppard01 <t.sheppard@kainos.com>
2017-10-17 14:58:46 +0100

    175: Refactors inserting offender manager, team, region etc out into service

---
+++ app/services/insert-workload-owner-and-dependencies.js
@@ -0,0 +1,68 @@
+const insertOffenderManagerTypeId = require('../data/insert-offender-manager-type-id')
+const insertOffenderManager = require('../data/insert-offender-manager')
+const insertWorkloadOwner = require('../data/insert-workload-owner')
+const insertTeam = require('../data/insert-team')
+const insertLdu = require('../data/insert-ldu')
+const insertRegion = require('../data/insert-region')
+
+const OffenderManager = require('wmt-probation-rules').OffenderManager
+const Team = require('wmt-probation-rules').Team
+const WorkloadOwner = require('wmt-probation-rules').WorkloadOwner
+const Ldu = require('wmt-probation-rules').Ldu
+const Region = require('wmt-probation-rules').Region
+const filterOmGradeCode = require('wmt-probation-rules').filterOmGradeCode
+
+module.exports = function (caseSummary) {
+  return insertOffenderManagerTypeId(caseSummary.omGradeCode)
+  .then(function (typeId) {
+    return insertOffenderManager(
+      new OffenderManager(
+        undefined,
+        caseSummary.omKey,
+        caseSummary.omForename,
+        caseSummary.omSurname,
+        typeId,
+        filterOmGradeCode(caseSummary.omGradeCode)
+      )
+    )
+    .then(function (offenderManagerId) {
+      return insertRegion(
+        new Region(
+          undefined,
+          caseSummary.regionCode,
+          caseSummary.regionDesc
+        )
+      )
+      .then(function (regionId) {
+        return insertLdu(
+          new Ldu(
+            undefined,
+            parseInt(regionId),
+            caseSummary.lduCode,
+            caseSummary.teamDesc
+          )
+        )
+        .then(function (lduId) {
+          return insertTeam(
+            new Team(
+              undefined,
+              parseInt(lduId),
+              caseSummary.teamCode,
+              caseSummary.teamDesc
+            )
+          )
+          .then(function (teamId) {
+            return insertWorkloadOwner(
+              new WorkloadOwner(
+                undefined,
+                parseInt(offenderManagerId),
+                undefined,
+                parseInt(teamId)
+              )
+            )
+          })
+        })
+      })
+    })
+  })
+}
--- app/services/workers/create-workload.js
+++ app/services/workers/create-workload.js
@@ -1,26 +1,15 @@
 const Promise = require('bluebird').Promise
 const logger = require('../log')
-const insertOffenderManagerTypeId = require('../data/insert-offender-manager-type-id')
-const insertOffenderManager = require('../data/insert-offender-manager')
-const insertWorkloadOwner = require('../data/insert-workload-owner')
-const insertTeam = require('../data/insert-team')
-const insertLdu = require('../data/insert-ldu')
-const insertRegion = require('../data/insert-region')
+const insertWorkloadOwnerAndDependencies = require('../insert-workload-owner-and-dependencies')
 const insertWorkload = require('../data/insert-app-workload')
 
-const OffenderManager = require('wmt-probation-rules').OffenderManager
-const Team = require('wmt-probation-rules').Team
-const WorkloadOwner = require('wmt-probation-rules').WorkloadOwner
 const mapWorkload = require('wmt-probation-rules').mapWorkload
-const Ldu = require('wmt-probation-rules').Ldu
-const Region = require('wmt-probation-rules').Region
 const Task = require('../domain/task')
 const taskType = require('../../constants/task-type')
 const taskStatus = require('../../constants/task-status')
 const submittingAgent = require('../../constants/task-submitting-agent')
 const getStagingWorkload = require('../data/get-staging-workload')
 const createNewTasks = require('../data/create-tasks')
-const filterOmGradeCode = require('wmt-probation-rules').filterOmGradeCode
 
 module.exports.execute = function (task) {
   var workloadBatchSize = task.additionalData.batchSize
@@ -32,54 +21,13 @@ module.exports.execute = function (task) {
     var insertedWorkloadIds = []
     return Promise.each(stagingWorkloads, function (stagingWorkload) {
       var caseSummary = stagingWorkload.casesSummary
-      return insertOffenderManagerTypeId(caseSummary.omGradeCode)
-          .then(function (typeId) {
-            return insertOffenderManager(new OffenderManager(
-                          undefined,
-                          caseSummary.omKey,
-                          caseSummary.omForename,
-                          caseSummary.omSurname,
-                          typeId,
-                          filterOmGradeCode(caseSummary.omGradeCode)
-                          ))
-          })
-      .then(function (offenderManagerId) {
-        return insertRegion(new Region(
-                      undefined,
-                      caseSummary.regionCode,
-                      caseSummary.regionDesc
-                      ))
-              .then(function (regionId) {
-                return insertLdu(new Ldu(
-                              undefined,
-                              parseInt(regionId),
-                              caseSummary.lduCode,
-                              caseSummary.teamDesc
-                              ))
-              })
-          .then(function (lduId) {
-            return insertTeam(new Team(
-                          undefined,
-                          parseInt(lduId),
-                          caseSummary.teamCode,
-                          caseSummary.teamDesc
-                          ))
-          })
-          .then(function (teamId) {
-            return insertWorkloadOwner(new WorkloadOwner(
-                          undefined,
-                          parseInt(offenderManagerId),
-                          undefined,
-                          parseInt(teamId)
-                          ))
-          })
-          .then(function (workloadOwnerId) {
-            var workloadToInsert = mapWorkload(stagingWorkload, parseInt(workloadOwnerId), parseInt(workloadReportId))
-            return insertWorkload(workloadToInsert)
-                .then(function (workloadId) {
-                  insertedWorkloadIds.push(workloadId)
-                })
-          })
+      return insertWorkloadOwnerAndDependencies(caseSummary)
+      .then(function (workloadOwnerId) {
+        var workloadToInsert = mapWorkload(stagingWorkload, parseInt(workloadOwnerId), parseInt(workloadReportId))
+        return insertWorkload(workloadToInsert)
+        .then(function (workloadId) {
+          insertedWorkloadIds.push(workloadId)
+        })
       })
     })
     .then(function () {
