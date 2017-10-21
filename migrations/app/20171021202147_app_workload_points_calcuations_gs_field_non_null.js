6e629412f8a82825456d13fbea76dcffcf33a5cf
Andrew Wright <a.wright@kainos.com>
2017-10-21 21:22:34 +0100

    Create indexed views for ldu and team capacity breakdowns

---
+++ migrations/app/20171021202147_app_workload_points_calcuations_gs_field_non_null.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+  var sql = "UPDATE app.workload_points_calculations SET gs_adjustment_points = 0 WHERE gs_adjustment_points IS NULL;"
+  sql += "UPDATE app.workload_points_calculations SET cms_adjustment_points = 0 WHERE cms_adjustment_points IS NULL;"
+  sql += "ALTER TABLE app.workload_points_calculations ALTER COLUMN gs_adjustment_points DECIMAL (5,2) NOT NULL;"
+  sql += "ALTER TABLE app.workload_points_calculations ALTER COLUMN cms_adjustment_points INT NOT NULL;"
+  return knex.schema
+        .raw('SET ARITHABORT ON')
+        .raw(sql)
+};
+
+exports.down = function(knex, Promise) {
+  var sql = "ALTER TABLE app.workload_points_calculations ALTER COLUMN gs_adjustment_points DECIMAL (5,2);"
+  var sql = "ALTER TABLE app.workload_points_calculations ALTER COLUMN gs_adjustment_points DECIMAL (5,2);"
+  return knex.schema
+        .raw('SET ARITHABORT ON')
+        .raw(sql)
+};
