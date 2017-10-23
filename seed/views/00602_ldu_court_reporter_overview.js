d4a2cb65c0a6743b524d4e7db16fa1bd93c7a8bc
Andrew Wright <a.wright@kainos.com>
2017-10-23 20:33:40 +0100

    Update court report overview views to use indexes.

--- seed/views/00602_ldu_court_reporter_overview.js
+++ seed/views/00602_ldu_court_reporter_overview.js
@@ -1,24 +1,35 @@
 exports.seed = function (knex, promise) {
-  var sql = `CREATE VIEW app.ldu_court_reporter_overview
+  var view = `CREATE VIEW app.ldu_court_reporter_overview
     WITH SCHEMABINDING
     AS
     SELECT
-      MAX(l.id) AS id
-      , MAX(t.description) AS name
-      , MAX(tv.id) AS link_id      
-      , MAX(tv.grade_code) AS grade_code
-      , MAX(tv.contracted_hours) AS contracted_hours
-      , MAX(tv.reduction_hours) AS reduction_hours
-      , SUM(tv.total_sdrs) AS total_sdrs
-      , SUM(tv.total_fdrs) AS total_fdrs
-      , SUM(tv.total_oral_reports) AS total_oral_reports
-    FROM app.team_court_reporter_overview tv
-      JOIN app.team t ON tv.id = t.id
-      JOIN app.ldu l ON l.id = t.ldu_id
-    GROUP BY tv.id;`
+        l.id
+      , t.description AS name
+      , t.id AS link_id
+      , SUM(crc.contracted_hours) AS contracted_hours
+      , SUM(crc.reduction_hours) AS reduction_hours
+      , SUM(cr.total_sdrs) AS total_sdrs
+      , SUM(cr.total_fdrs) AS total_fdrs
+      , SUM(cr.total_oral_reports) AS total_oral_reports
+      , COUNT_BIG(*) AS count
+    FROM app.workload_owner wo
+        JOIN app.team t ON wo.team_id = t.id
+        JOIN app.offender_manager om ON om.id = wo.offender_manager_id
+        JOIN app.offender_manager_type om_type ON om_type.id = om.type_id
+        JOIN app.court_reports cr ON wo.id = cr.workload_owner_id
+        JOIN app.court_reports_calculations crc ON crc.court_reports_id = cr.id
+        JOIN app.workload_report wr ON wr.id = crc.workload_report_id
+        JOIN app.ldu l ON l.id = t.ldu_id
+    WHERE wr.effective_from IS NOT NULL
+        AND wr.effective_to IS NULL
+    GROUP BY l.id, t.id, t.description;`
+
+  var index = `CREATE UNIQUE CLUSTERED INDEX idx_ldu_court_reporter_overview
+  ON app.ldu_court_reporter_overview (link_id)`
 
   return knex.schema
       .raw('DROP VIEW IF EXISTS app.ldu_court_reporter_overview;')
       .raw('SET ARITHABORT ON')
-      .raw(sql)
+      .raw(view)
+      .raw(index)
 }
--- seed/views/00603_region_court_reporter_overview.js
+++ seed/views/00603_region_court_reporter_overview.js
@@ -1,24 +1,36 @@
 exports.seed = function (knex, promise) {
-  var sql = `CREATE VIEW app.region_court_reporter_overview
+  var view = `CREATE VIEW app.region_court_reporter_overview
     WITH SCHEMABINDING
     AS
     SELECT
-      MAX(r.id) AS id
-      , MAX(l.description) AS name
-      , MAX(lv.id) AS link_id      
-      , MAX(lv.grade_code) AS grade_code
-      , MAX(lv.contracted_hours) AS contracted_hours
-      , MAX(lv.reduction_hours) AS reduction_hours
-      , SUM(lv.total_sdrs) AS total_sdrs
-      , SUM(lv.total_fdrs) AS total_fdrs
-      , SUM(lv.total_oral_reports) AS total_oral_reports
-    FROM app.ldu_court_reporter_overview lv
-      JOIN app.ldu l ON lv.id = l.id
-      JOIN app.region r ON r.id = l.region_id
-    GROUP BY lv.id;`
+        r.id
+      , l.description AS name
+      , l.id AS link_id
+      , SUM(crc.contracted_hours) AS contracted_hours
+      , SUM(crc.reduction_hours) AS reduction_hours
+      , SUM(cr.total_sdrs) AS total_sdrs
+      , SUM(cr.total_fdrs) AS total_fdrs
+      , SUM(cr.total_oral_reports) AS total_oral_reports
+      , COUNT_BIG(*) AS count
+    FROM app.workload_owner wo
+        JOIN app.team t ON wo.team_id = t.id
+        JOIN app.offender_manager om ON om.id = wo.offender_manager_id
+        JOIN app.offender_manager_type om_type ON om_type.id = om.type_id
+        JOIN app.court_reports cr ON wo.id = cr.workload_owner_id
+        JOIN app.court_reports_calculations crc ON crc.court_reports_id = cr.id
+        JOIN app.workload_report wr ON wr.id = crc.workload_report_id
+        JOIN app.ldu l ON l.id = t.ldu_id
+        JOIN app.region r ON r.id = l.region_id
+    WHERE wr.effective_from IS NOT NULL
+        AND wr.effective_to IS NULL
+    GROUP BY r.id, l.id, l.description;`
+
+  var index = `CREATE UNIQUE CLUSTERED INDEX idx_region_court_reporter_overview
+  ON app.region_court_reporter_overview (link_id)`
 
   return knex.schema
       .raw('DROP VIEW IF EXISTS app.region_court_reporter_overview;')
       .raw('SET ARITHABORT ON')
-      .raw(sql)
+      .raw(view)
+      .raw(index)
 }
