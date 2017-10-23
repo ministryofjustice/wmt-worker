425f124b9507118ec83ae896cdf1d60263025858
Andrew Wright <a.wright@kainos.com>
2017-10-23 23:37:49 +0100

    Add forename and surname to the caseload progress views
    This removes the need for an extra call from the web app to retrieve the OM name
    for each row.

--- seed/views/00200_individual_case_progress_view.js
+++ seed/views/00200_individual_case_progress_view.js
@@ -3,24 +3,28 @@ exports.seed = function (knex, Promise) {
   WITH SCHEMABINDING
   AS
   SELECT 
-      w.workload_owner_id AS workload_owner_id
+      om.forename
+    , om.surname
     , w.community_last_16_weeks AS community_last_16_weeks
     , w.license_last_16_weeks AS license_last_16_weeks
     , w.total_cases AS total_cases
     , SUM(tr.warrants_total) AS warrants_total
     , SUM(tr.overdue_terminations_total) AS overdue_terminations_total
     , SUM(tr.unpaid_work_total) AS unpaid_work_total
-    , w.workload_owner_id AS id
+    , wo.id AS id
     , COUNT_BIG(*) AS count
   FROM app.workload w
     JOIN app.workload_points_calculations wpc ON wpc.workload_id = w.id
     JOIN app.workload_report wr ON wr.id = wpc.workload_report_id
     JOIN app.tiers tr ON tr.workload_id = w.id
     JOIN app.workload_owner wo ON wo.id = w.workload_owner_id
+    JOIN app.offender_manager om ON om.id = wo.offender_manager_id
   WHERE wr.effective_from IS NOT NULL
   AND wr.effective_to IS NULL
   GROUP BY
-      w.workload_owner_id
+      om.forename
+    , om.surname
+    , wo.id
     , w.community_last_16_weeks
     , w.license_last_16_weeks
     , w.total_cases;`
