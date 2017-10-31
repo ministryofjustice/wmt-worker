0e547ffd86ced3bfd017ae7df5c3b51f26a8fa76
Caoimhe Boyle <c.boyle@kainos.com>
2017-10-31 14:36:47 +0000

    408: Add staging table for wmt_extract_sa tab

---
+++ migrations/staging/20171031131614_stg_wmt_extract_sa.js
@@ -0,0 +1,21 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.withSchema('staging').createTable('wmt_extract_sa', function (table) {
+    table.increments('id')
+    table.string('case_ref_no')
+    table.string('tier_code')
+    table.string('team_code')
+    table.string('om_grade_code')
+    table.string('om_key')
+    table.string('location')
+    table.string('disposal_type_desc')
+    table.string('disposal_type_code')
+    table.string('standalone_order')
+  }).catch(function (error) {
+    console.log(error)
+    throw error
+  })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.withSchema('staging').dropTable('wmt_extract_sa')
+}
