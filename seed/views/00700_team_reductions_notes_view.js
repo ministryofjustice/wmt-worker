bc90c17b72fb02c7a7e1def956090d5345d50577
David McKinney <d.mckinney@kainos.com>
2017-11-01 16:08:47 +0000

    Create reductions export view without index

--- seed/views/00700_team_reductions_notes_view.js
+++ seed/views/00700_team_reductions_notes_view.js
@@ -14,6 +14,9 @@ exports.seed = function (knex, promise) {
         , r.notes AS additional_notes
       FROM app.workload_owner wo
           JOIN app.team t ON wo.team_id = t.id
+          JOIN app.workload w ON wo.id = w.workload_owner_id
+          JOIN app.workload_points_calculations wpc ON wpc.workload_id = w.id
+          JOIN app.workload_report wr ON wr.id = wpc.workload_report_id
           JOIN app.offender_manager om ON om.id = wo.offender_manager_id
           JOIN app.reductions r ON r.workload_owner_id = wo.id
           JOIN app.reduction_reason rr ON r.reduction_reason_id = rr.id
@@ -21,12 +24,12 @@ exports.seed = function (knex, promise) {
           AND wr.effective_to IS NULL
       ;`
   
-    var index = `CREATE UNIQUE CLUSTERED INDEX idx_team_reductions_notes_view
-    ON app.team_reductions_notes_view (link_id)`
+    //var index = `CREATE UNIQUE CLUSTERED INDEX idx_team_reductions_notes_view
+    //ON app.team_reductions_notes_view (workload_owner_id)`
   
     return knex.schema
         .raw('DROP VIEW IF EXISTS app.team_reductions_notes_view;')
         .raw('SET ARITHABORT ON')
         .raw(view)
-        .raw(index)
+        //.raw(index)
   }
