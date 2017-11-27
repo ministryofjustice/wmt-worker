c868a77a35ab3fbeeecb0cca64a5f74839353e22
ddebella <25299873+ddebella@users.noreply.github.com>
2017-11-27 16:16:48 +0000

    Fix issue with refreshing with empty ip address value.

--- app/services/refresh-web-org-hierarchy.js
+++ app/services/refresh-web-org-hierarchy.js
@@ -1,19 +1,22 @@
-const ipAdresses = require('../../config').IP_ADDRESSES
+const ipAddresses = require('../../config').IP_ADDRESSES
 const createRequestPromise = require('request-promise')
 const log = require('./log')
 const Promise = require('bluebird').Promise
 
 module.exports = function () {
-  var ipAddressesArray = ipAdresses.split(',')
+  var ipAddressesArray = []
+  if (ipAddresses !== undefined) {
+    ipAddressesArray = ipAddresses.split(',')
+  }
 
   return Promise.each(ipAddressesArray, function (ipAddress) {
     return createRequestPromise(ipAddress + '/refresh')
-    .then(function () {
-      log.info(ipAddress + ' successfully refreshed')
-    }).catch(function (err) {
-      log.error('Cannot refresh hierarchy for ' + ipAddress)
-      log.error(err)
-      throw new Error(err)
-    })
+      .then(function () {
+        log.info(ipAddress + ' successfully refreshed')
+      }).catch(function (err) {
+        log.error('Cannot refresh hierarchy for ' + ipAddress)
+        log.error(err)
+        throw new Error(err)
+      })
   })
 }
