ceb1dce1fda089feb09a2be8bdaa0eef292e4d14
ddebella <25299873+ddebella@users.noreply.github.com>
2017-12-04 17:18:31 +0000

    Type in the constraint name

--- migrations/app/20171204150838_app_reductions_add_audit_trail.js
+++ migrations/app/20171204150838_app_reductions_add_audit_trail.js
@@ -1,5 +1,5 @@
 exports.up = function (knex, Promise) {
-  var sql = 'ALTER TABLE app.reductions ADD updated_date DATETIME NOT NULL CONSTRAINT pdated_date_default DEFAULT GETDATE();'
+  var sql = 'ALTER TABLE app.reductions ADD updated_date DATETIME NOT NULL CONSTRAINT updated_date_default DEFAULT GETDATE();'
   sql += 'ALTER TABLE app.reductions ADD user_id INT NULL;'
   return knex.schema
               .raw('SET ARITHABORT ON')
