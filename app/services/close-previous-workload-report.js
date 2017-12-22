ef1273381aa5aca75545294e28c546f8b0f95c4f
David McKinney <d.mckinney@kainos.com>
2017-12-22 12:00:45 +0000

    Prevent undefined errors arising from close previous workload report

--- app/services/close-previous-workload-report.js
+++ app/services/close-previous-workload-report.js
@@ -4,12 +4,19 @@ const updateWorkloadReportEffectiveEndDate = require('./data/update-workload-rep
 module.exports = function (newWorkloadReportId) {
   return getOpenWorkloadReports()
     .then(function (results) {
+      var id = 0
+      var effectiveFrom = new Date()
       var currentWorkload = results.filter((item) => item.id !== newWorkloadReportId)
       var newWorkload = results.filter((item) => item.id === newWorkloadReportId)
-
-      return updateWorkloadReportEffectiveEndDate(currentWorkload[0].id, newWorkload[0].effective_from)
+      if (currentWorkload && currentWorkload.length > 0) {
+        id = currentWorkload[0].id
+      }
+      if (newWorkload && newWorkload.length > 0) {
+        effectiveFrom = newWorkload[0].effective_from
+      }
+      return updateWorkloadReportEffectiveEndDate(id, effectiveFrom)
       .then(function () {
-        return currentWorkload[0].id
+        return id
       })
     })
 }
