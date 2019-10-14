fe1729dfb0a52fe82fd6be215ce24bedef260a4b
kevinfox <kevinfox@kainos.com>
2019-10-14 17:37:27 +0100

    WMT0115: Added an integration test to test filtered totals are inserted properly

---
+++ test/helpers/data/delete-staging-records.js
@@ -0,0 +1,12 @@
+const knex = require('../../../knex').stagingSchema
+
+module.exports = function () {
+  return knex('wmt_extract').del()
+    .then(function () { return knex('wmt_extract_filtered').del() })
+    .then(function () { return knex('flag_o_due').del() })
+    .then(function () { return knex('flag_priority').del() })
+    .then(function () { return knex('flag_upw').del() })
+    .then(function () { return knex('flag_warr_4_n').del() })
+    .then(function () { return knex('wmt_extract_sa').del() })
+    .then(function () { return knex('suspended_lifers').del() })
+}
---
+++ test/helpers/data/insert-staging-workload-helper.js
@@ -0,0 +1,51 @@
+const knex = require('../../../knex').stagingSchema
+const wmtExtract = require('../../constants/wmt-extract')
+const wmtExtractFiltered = require('../../constants/wmt-extract-filtered')
+const overdue = require('../../constants/flag-o-due')
+const priority = require('../../constants/flag-priority')
+const upw = require('../../constants/flag-upw')
+const warrants = require('../../constants/flag-warr-4-n')
+const suspendedSentenceOrders = require('../../constants/wmt-extract-sa')
+const suspendedLifers = require('../../constants/suspended-lifers')
+const deleteStagingRecords = require('./delete-staging-records')
+var inserts = []
+var stagingId
+
+module.exports = function () {
+  return deleteStagingRecords()
+    .then(function () {
+      return knex('wmt_extract')
+      .insert(wmtExtract)
+      .returning('id')
+      .then(function (id) {
+        stagingId = id
+        return knex('wmt_extract_filtered')
+          .insert(wmtExtractFiltered)
+      })
+    })
+    .then(function () {
+      overdue.forEach(function (record) {
+        inserts.push(insertToStagingTable('flag_o_due', record))
+      })
+      priority.forEach(function (record) {
+        inserts.push(insertToStagingTable('flag_priority', record))
+      })
+      upw.forEach(function (record) {
+        inserts.push(insertToStagingTable('flag_upw', record))
+      })
+      warrants.forEach(function (record) {
+        inserts.push(insertToStagingTable('flag_warr_4_n', record))
+      })
+      suspendedSentenceOrders.forEach(function (record) {
+        inserts.push(insertToStagingTable('wmt_extract_sa', record))
+      })
+      suspendedLifers.forEach(function (record) {
+        inserts.push(insertToStagingTable('suspended_lifers', record))
+      })
+      return Promise.all(inserts).then(function () { return stagingId})
+    })
+}
+
+var insertToStagingTable = function (table, data) {
+  return knex(table).insert(data)
+}
