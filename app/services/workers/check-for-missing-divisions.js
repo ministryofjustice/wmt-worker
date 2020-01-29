b1483b424eb21af99971c69348bb85ccb731a598
kevinfox <kevinfox@kainos.com>
2020-01-29 12:25:44 +0000

    WMT0180: Fixing an issue with the database object returned

--- app/services/workers/check-for-missing-divisions.js
+++ app/services/workers/check-for-missing-divisions.js
@@ -4,9 +4,9 @@ const EXPECTED_DIVISION_COUNT = require('../../../config').EXPECTED_DIVISIONS_CO
 
 module.exports.execute = function (task) {
   return getDivisionsDisplayedCount()
-    .then(function (result) {
-      if (result.divisionCount !== parseInt(EXPECTED_DIVISION_COUNT)) {
-        logger.error('ERROR: Check For Missing Divisions - Missing Divisions Found. Expected ' + EXPECTED_DIVISION_COUNT + ' but found ' + result.divisionCount)
+    .then(function (results) {
+      if (results[0].divisionCount !== parseInt(EXPECTED_DIVISION_COUNT)) {
+        logger.error('ERROR: Check For Missing Divisions - Missing Divisions Found. Expected ' + EXPECTED_DIVISION_COUNT + ' but found ' + results[0].divisionCount)
       }
     })
 }
