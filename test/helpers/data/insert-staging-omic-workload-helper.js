20facb9a3932bb160e91732bd549e59f1a46e1ca
kevinfox <kevinfox@kainos.com>
2020-03-24 13:28:33 +0000

    WMT0172: Altering Omic Workload Integration tests

--- test/helpers/data/insert-staging-omic-workload-helper.js
+++ test/helpers/data/insert-staging-omic-workload-helper.js
@@ -1,48 +1,17 @@
 const knex = require('../../../knex').stagingSchema
-const wmtExtract = require('../../constants/wmt-extract')
-const overdue = require('../../constants/flag-o-due')
-const priority = require('../../constants/flag-priority')
-const upw = require('../../constants/flag-upw')
-const warrants = require('../../constants/flag-warr-4-n')
-const suspendedSentenceOrders = require('../../constants/wmt-extract-sa')
-const suspendedLifers = require('../../constants/suspended-lifers')
+const omicWmtExtract = require('../../constants/omic-wmt-extract')
 const deleteStagingOmicRecords = require('./delete-staging-omic-records')
-var inserts = []
 var stagingId
 
 module.exports = function () {
   return deleteStagingOmicRecords()
     .then(function () {
       return knex('omic_wmt_extract')
-      .insert(wmtExtract)
+      .insert(omicWmtExtract)
       .returning('id')
       .then(function (id) {
         stagingId = id
+        return stagingId
       })
     })
-    .then(function () {
-      overdue.forEach(function (record) {
-        inserts.push(insertToStagingTable('flag_o_due', record))
-      })
-      priority.forEach(function (record) {
-        inserts.push(insertToStagingTable('flag_priority', record))
-      })
-      upw.forEach(function (record) {
-        inserts.push(insertToStagingTable('flag_upw', record))
-      })
-      warrants.forEach(function (record) {
-        inserts.push(insertToStagingTable('flag_warr_4_n', record))
-      })
-      suspendedSentenceOrders.forEach(function (record) {
-        inserts.push(insertToStagingTable('wmt_extract_sa', record))
-      })
-      suspendedLifers.forEach(function (record) {
-        inserts.push(insertToStagingTable('suspended_lifers', record))
-      })
-      return Promise.all(inserts).then(function () { return stagingId })
-    })
-}
-
-var insertToStagingTable = function (table, data) {
-  return knex(table).insert(data)
 }
