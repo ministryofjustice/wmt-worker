423725f8d6c89b5e3727effb1dba313777ade61e
kevinfox <kevinfox@kainos.com>
2021-06-04 11:42:07 +0100

    WMT-34: Importing dates into staging schema as yyyy-mm-dd hh:mm:ss for compatibility

--- test/helpers/etl/get-extract-file-data.js
+++ test/helpers/etl/get-extract-file-data.js
@@ -7,15 +7,15 @@ module.exports = function () {
   const workbooks = []
   const worksheets = {}
   extractFiles.forEach(function (extractFile) {
-    const workbook = XLSX.readFile(extractFile)
+    const workbook = XLSX.readFile(extractFile, { type: 'binary', cellText: false, cellDates: true })
     workbooks.push(workbook)
   })
 
   config.VALID_SHEET_NAMES.forEach(function (sheet) {
     const worksheet1 = workbooks[0].Sheets[sheet]
     const worksheet2 = workbooks[1].Sheets[sheet]
-    const worksheet1DataAsJSON = XLSX.utils.sheet_to_json(worksheet1, { raw: false, defval: null })
-    const worksheet2DataAsJSON = XLSX.utils.sheet_to_json(worksheet2, { raw: false, defval: null })
+    const worksheet1DataAsJSON = XLSX.utils.sheet_to_json(worksheet1, { raw: false, defval: null, dateNF: 'yyyy-mm-dd hh:mm:ss' })
+    const worksheet2DataAsJSON = XLSX.utils.sheet_to_json(worksheet2, { raw: false, defval: null, dateNF: 'yyyy-mm-dd hh:mm:ss' })
     worksheets[sheet] = worksheet1DataAsJSON.concat(worksheet2DataAsJSON)
   })
   return worksheets
