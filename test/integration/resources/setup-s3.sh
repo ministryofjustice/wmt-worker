be625b10d102f7e3dd544f9c13d41cca806fec12
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-07-08 14:35:03 +0100

    WFP-325 parameterise excel location for s3 bucket

--- test/integration/resources/setup-s3.sh
+++ test/integration/resources/setup-s3.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+echo 'if you want to run this outside docker do ./setup-s3.sh $(PWD)'
+
+excelPath=${1:-/docker-entrypoint-initaws.d}
+
 set -e
 export TERM=ansi
 export AWS_ACCESS_KEY_ID=foobar
@@ -7,5 +11,5 @@ export AWS_DEFAULT_REGION=eu-west-2
 export PAGER=
 
 aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket wmt-worker
-aws --endpoint-url=http://localhost:4566 s3api put-object --bucket wmt-worker --key extract/etl-example.xlsx --body /docker-entrypoint-initaws.d/etl-example.xlsx
+aws --endpoint-url=http://localhost:4566 s3api put-object --bucket wmt-worker --key extract/etl-example.xlsx --body $excelPath/etl-example.xlsx
 echo "S3 created bucket"
