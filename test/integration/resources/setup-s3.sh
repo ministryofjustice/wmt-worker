aa2fb51c773451b20d9efd0e4449258dc321bb33
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-08-03 10:05:17 +0100

    WFP-440 set up notification from S3 extract bucket to SQS

--- test/integration/resources/setup-s3.sh
+++ test/integration/resources/setup-s3.sh
@@ -11,6 +11,11 @@ export AWS_DEFAULT_REGION=eu-west-2
 export PAGER=
 
 aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket wmt-worker
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name manage-a-sentence-development-hmpps_workload_s3_extract_event_queue
+aws --endpoint-url=http://localhost:4566 s3api put-bucket-notification-configuration --bucket wmt-worker --notification-configuration '{"QueueConfigurations":[{"QueueArn":"arn:aws:sqs:eu-west-2:000000000000:manage-a-sentence-development-hmpps_workload_s3_extract_event_queue","Events": ["s3:ObjectCreated:*"]}]}'
+
+
 aws --endpoint-url=http://localhost:4566 s3api put-object --bucket wmt-worker --key extract/etl-example.xlsx --body $excelPath/etl-example.xlsx
 aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket wmt-worker-dashboard
+
 echo "S3 created bucket"
