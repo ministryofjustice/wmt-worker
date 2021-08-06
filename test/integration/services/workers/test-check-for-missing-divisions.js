4959ebf8c90c6623c5e6eacbed69c2a2b6f75901
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-08-06 15:29:04 +0100

    WFP-424 make job under test succeed

--- test/integration/services/workers/test-check-for-missing-divisions.js
+++ test/integration/services/workers/test-check-for-missing-divisions.js
@@ -1,11 +1,13 @@
+const proxyquire = require('proxyquire')
 const expect = require('chai').expect
-const checkForMissingDivisions = require('../../../../app/services/workers/check-for-missing-divisions')
 const knex = require('../../../../knex').appSchema
 const taskType = require('../../../../app/constants/task-type')
 const taskStatus = require('../../../../app/constants/task-status')
 
 describe('services/workers/check-for-missing-divisions', function () {
   it('should create a generate dashboard task once completed', function () {
+    const checkForMissingDivisions = proxyquire('../../../../app/services/workers/check-for-missing-divisions', { '../../../config': { EXPECTED_DIVISIONS_COUNT: '0' } })
+
     return checkForMissingDivisions.execute().then(function (result) {
       expect(result).to.be.an('array')
       expect(result).to.have.lengthOf(1)
