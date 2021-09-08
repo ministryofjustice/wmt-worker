6f1fa41eb78c86981b17e6e0909212cfee081971
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-09-08 12:11:23 +0100

    WFp-470 log delete duplicate cms to see if it ever happens

--- app/services/data/delete-duplicate-cms.js
+++ app/services/data/delete-duplicate-cms.js
@@ -1,5 +1,6 @@
 const knex = require('../../../knex').appSchema
 const { arrayToPromise } = require('../helpers/promise-helper')
+const log = require('../log')
 
 module.exports = function (contactId) {
   return knex.select(knex.raw('count(*) AS theCount'), 'contact_id AS contactId', 'workload_owner_id AS workloadOwnerId', 'points')
@@ -10,8 +11,9 @@ module.exports = function (contactId) {
     .having(knex.raw('count(*)'), '>', 1)
     .then(function (results) {
       return arrayToPromise(results, function (result) {
-        let count = 0
-        count = result.theCount - 1
+        const count = result.theCount - 1
+        log.info('Deleting ' + count + ' adjustments for contact ' + result.contactId + ' and  workload owner ' + result.workloadOwnerId + ' and points ' + result.points)
+
         return knex.schema.raw('DELETE TOP (' + count + ') FROM app.adjustments WHERE contact_id = ' + result.contactId + ' AND workload_owner_id = ' + result.workloadOwnerId + ' AND points = ' + result.points)
       })
     })
