94027b0ce9895eb800ce8c5d7c45244c5fc36d81
carlov20 <carlov20@users.noreply.github.com>
2021-09-22 15:13:14 +0100

    fix

--- app/services/record-etl-execution-time.js
+++ app/services/record-etl-execution-time.js
@@ -4,6 +4,6 @@ const log = require('./log')
 module.exports = function (newWorkloadReportId) {
   return getWorkloadReportById(newWorkloadReportId)
     .then(function (result) {
-      log.trackExecutionTime('NART Extract', new Date().getTime() - new Date(result[0].effectiveFrom).getTime(), true)
+      log.trackExecutionTime('NART Extract', new Date().getTime() - new Date(result[0].effective_from).getTime(), true)
     })
 }
