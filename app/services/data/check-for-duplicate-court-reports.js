9a25ecc058d0ae53d8c3d07609c3f7d304ea4b71
carlov20 <carlov20@users.noreply.github.com>
2021-09-22 10:45:19 +0100

    WFP-568 updated duplicate check queries

--- app/services/data/check-for-duplicate-court-reports.js
+++ app/services/data/check-for-duplicate-court-reports.js
@@ -1,9 +1,14 @@
 const knex = require('../../../knex').appSchema
+const workloadReportStatus = require('../../constants/workload-report-status')
 
 module.exports = function () {
-  return knex('individual_court_reporter_overview')
+  return knex('workload_owner')
     .withSchema('app')
-    .select('id')
-    .groupBy('id')
-    .having(knex.raw('count(id)'), '>', 1)
+    .select('workload_owner.id as id')
+    .join('court_reports', 'workload_owner.id', 'court_reports.workload_owner_id')
+    .join('court_reports_calculations', 'court_reports_calculations.court_reports_id', 'court_reports.id')
+    .join('workload_report', 'workload_report.id', 'court_reports_calculations.workload_report_id')
+    .where('workload_report.status', workloadReportStatus.INPROGRESS)
+    .groupBy('workload_owner.id')
+    .having(knex.raw('count(workload_owner.id)'), '>', 1)
 }
--- app/services/data/check-for-duplicate-workloads.js
+++ app/services/data/check-for-duplicate-workloads.js
@@ -1,9 +1,14 @@
 const knex = require('../../../knex').appSchema
+const workloadReportStatus = require('../../constants/workload-report-status')
 
 module.exports = function () {
-  return knex('team_capacity_breakdown_view')
+  return knex('workload_points_calculations')
     .withSchema('app')
-    .select('link_id')
-    .groupBy('link_id')
-    .having(knex.raw('count(link_id)'), '>', 1)
+    .select('workload_owner.id AS link_id')
+    .join('workload', 'workload_points_calculations.workload_id', 'workload.id')
+    .join('workload_owner', 'workload.workload_owner_id', 'workload_owner.id')
+    .join('workload_report', 'workload_points_calculations.workload_report_id', 'workload_report.id')
+    .where('workload_report.status', workloadReportStatus.INPROGRESS)
+    .groupBy('workload_owner.id')
+    .having(knex.raw('count(workload_owner.id)'), '>', 1)
 }
