ddfedc22f8b44b506148c1f70e2732fdc1798b54
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-10-01 08:40:59 +0100

    FIXBUILD: ignore failed and cancelled tasks when running ETL

--- app/services/data/get-tasks-not-complete-count.js
+++ app/services/data/get-tasks-not-complete-count.js
@@ -1,8 +1,8 @@
 const knex = require('../../../knex').appSchema
-const taskStatus = require('../../constants/task-status')
+const { COMPLETE, FAILED, CANCELLED } = require('../../constants/task-status')
 
 module.exports = function () {
   return knex('tasks').withSchema('app')
-    .where('status', '!=', taskStatus.COMPLETE)
+    .whereNotIn('status', [COMPLETE, FAILED, CANCELLED])
     .count('* AS theCount')
 }
