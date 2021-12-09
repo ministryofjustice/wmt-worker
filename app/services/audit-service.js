5f5e3986c4032ac504110c98efaa0a1284a7bd6c
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-12-09 07:38:38 +0000

    WFP-809 write audit to queue

--- app/services/audit-service.js
+++ app/services/audit-service.js
@@ -1,15 +1,31 @@
-// const { AUDIT_SQS } = require('../../etl-config')
-// const { ACTIVE, ARCHIVED } = require('../constants/reduction-status')
+const { AUDIT_SQS } = require('../../etl-config')
+const { ACTIVE, ARCHIVED } = require('../constants/reduction-status')
 
-// const getSqsClient = require('./aws/sqs/get-sqs-client')
+const getSqsClient = require('./aws/sqs/get-sqs-client')
+const sendSqsMessage = require('./aws/sqs/send-sqs-message')
 
-// const reductionStatusToAuditAction = {
-//   [ACTIVE]: 'REDUCTION_STARTED',
-//   [ARCHIVED]: 'REDUCTION_ENDED'
-// }
+const reductionStatusToAuditAction = {
+  [ACTIVE]: 'REDUCTION_STARTED',
+  [ARCHIVED]: 'REDUCTION_ENDED'
+}
 
-// const sqsClient = getSqsClient({ region: AUDIT_SQS.REGION, accessKeyId: AUDIT_SQS.ACCESS_KEY_ID, secretAccessKey: AUDIT_SQS.SECRET_ACCESS_KEY, endpoint: AUDIT_SQS.ENDPOINT })
+const sqsClient = getSqsClient({ region: AUDIT_SQS.REGION, accessKeyId: AUDIT_SQS.ACCESS_KEY_ID, secretAccessKey: AUDIT_SQS.SECRET_ACCESS_KEY, endpoint: AUDIT_SQS.ENDPOINT })
 
 module.exports.auditReductionStatusChange = function (records, newStatus) {
-  return Promise.resolve()
+  const audits = []
+  records.forEach(function (record) {
+    audits.push(sendSqsMessage(sqsClient, AUDIT_SQS.QUEUE_URL, messageFrom(record, newStatus)))
+  })
+  return Promise.all(audits)
+}
+
+function messageFrom (record, status) {
+  return JSON.stringify({
+    what: reductionStatusToAuditAction[status],
+    when: new Date(),
+    operationId: 'TODO',
+    who: 'system worker',
+    service: 'wmt',
+    details: JSON.stringify({ ...record, status })
+  })
 }
