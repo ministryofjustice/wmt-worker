81ffd54d22102d7cca1d63b828af76dfe03a6b89
carlov20 <carlov20@users.noreply.github.com>
2022-08-16 13:22:39 +0100

    WFP-1307 added refreshing cms export view

--- app/services/data/refresh-materialized-views.js
+++ app/services/data/refresh-materialized-views.js
@@ -4,4 +4,9 @@ module.exports = function () {
   return knex
     .schema
     .raw('REFRESH MATERIALIZED VIEW app.case_details_export_view')
+    .then(function () {
+      return knex
+        .schema
+        .raw('REFRESH MATERIALIZED VIEW app.cms_export_view')
+    })
 }
