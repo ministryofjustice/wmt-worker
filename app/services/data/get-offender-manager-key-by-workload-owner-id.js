f08233620794dac71a4223b3a80dd1ff5a9f0d61
carlov20 <carlov20@users.noreply.github.com>
2022-10-04 10:49:58 +0100

    fixed query

--- app/services/data/get-offender-manager-key-by-workload-owner-id.js
+++ app/services/data/get-offender-manager-key-by-workload-owner-id.js
@@ -3,7 +3,7 @@ const knex = require('../../../knex').appSchema
 module.exports = function (workloadOwnerId) {
   return knex('workload_owner').withSchema('app')
     .join('offender_manager', 'workload_owner.offender_manager_id', '=', 'offender_manager.id')
-    .join('team', 'workload_owner.team', '=', 'team.id')
+    .join('team', 'workload_owner.team_id', '=', 'team.id')
     .where('workload_owner.id', workloadOwnerId)
     .first('offender_manager.key', 'team.code')
     .then((result) => ({ staffCode: result.key, teamCode: result.code }))
