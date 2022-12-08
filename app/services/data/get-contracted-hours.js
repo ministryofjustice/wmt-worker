640ff3e247c5bb5eeb927679e049efe0ce59a2d5
carlov20 <carlov20@users.noreply.github.com>
2022-12-08 13:59:07 +0000

    removed workload points join when retrieving contracted hours

--- app/services/data/get-contracted-hours.js
+++ app/services/data/get-contracted-hours.js
@@ -2,18 +2,7 @@ const knex = require('../../../knex').appSchema
 
 module.exports = function (workloadOwnerId) {
   return knex('workload_owner').withSchema('app')
-    .crossJoin('workload_points')
-    .whereNull('workload_points.effective_to')
-    .andWhere('workload_owner.id', workloadOwnerId)
-    .andWhere('is_t2a', false)
-    .first('workload_owner.contracted_hours AS contracted_hours',
-      'workload_points.default_contracted_hours_po AS default_hours'
-    )
-    .then((result) => {
-      let hours = result.contracted_hours
-      if (hours === null) {
-        hours = result.default_hours
-      }
-      return hours
-    })
+    .where('workload_owner.id', workloadOwnerId)
+    .first('workload_owner.contracted_hours AS contractedHours')
+    .then(({ contractedHours }) => contractedHours)
 }
--- test/integration/services/data/test-get-contracted-hours.js
+++ test/integration/services/data/test-get-contracted-hours.js
@@ -14,21 +14,10 @@ describe('services/data/get-contracted-hours', function () {
       })
   })
 
-  it('should retrieve the contracted hours for a workload owner with specified contracted hours', function (done) {
+  it('should retrieve the contracted hours for a workload owner with specified contracted hours', function () {
     const workloadOwnerId = inserts.filter((item) => item.table === 'workload_owner')[0].id
-    getContractedHours(workloadOwnerId).then(function (hours) {
+    return getContractedHours(workloadOwnerId).then(function (hours) {
       expect(hours).to.equal(40)
-      done()
-    }).catch(function (err) {
-      done(err)
-    })
-  })
-
-  it('should retrieve default for a workload owner with no defined contracted hours', function (done) {
-    const workloadOwnerId = inserts.filter((item) => item.table === 'workload_owner')[1].id
-    getContractedHours(workloadOwnerId).then(function (hours) {
-      expect(hours).to.equal(37)
-      done()
     })
   })
 
