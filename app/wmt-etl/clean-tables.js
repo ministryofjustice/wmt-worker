d391d9e3509bc8718eb93f107d18093700ba483d
carlov20 <carlov20@users.noreply.github.com>
2023-01-09 10:06:54 +0000

    delete all data from arms and inst reports staging tables

--- app/wmt-etl/clean-tables.js
+++ app/wmt-etl/clean-tables.js
@@ -4,7 +4,14 @@ const { arrayToPromise } = require('../services/helpers/promise-helper')
 module.exports = function () {
   return arrayToPromise(config.VALID_SHEET_NAMES, function (table) {
     return knex(table).withSchema('staging').del()
-  }).then(function () {
+  })
+  .then(function () {
+    return knex('arms').withSchema('staging').del()
+  })
+  .then(function () {
+    return knex('inst_reports').withSchema('staging').del()
+  })
+  .then(function () {
     return knex('tasks').withSchema('app').del()
   }).then(function () {
     return knex('tiers').withSchema('app').truncate()
