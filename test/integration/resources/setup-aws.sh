191866dc9d8656813d3ecdd1ec9b3408369e765d
carlov20 <carlov20@users.noreply.github.com>
2023-03-30 11:20:36 +0100

    switched to sqs queue arn

--- test/integration/resources/setup-aws.sh
+++ test/integration/resources/setup-aws.sh
@@ -7,23 +7,23 @@ export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
 export PAGER=
 
-aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket wmt-worker --region ${AWS_DEFAULT_REGION} --create-bucket-configuration LocationConstraint=${AWS_DEFAULT_REGION}
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name s3_extract_event_queue
+aws --endpoint-url=http://127.0.0.1:4566 s3api create-bucket --bucket wmt-worker --region ${AWS_DEFAULT_REGION} --create-bucket-configuration LocationConstraint=${AWS_DEFAULT_REGION}
+aws --endpoint-url=http://127.0.0.1:4566 sqs create-queue --queue-name s3_extract_event_queue
 
-aws --endpoint-url=http://localhost:4566 s3api put-bucket-notification-configuration --bucket wmt-worker --notification-configuration '{"QueueConfigurations":[{"QueueArn":"arn:aws:sqs:eu-west-2:000000000000:s3_extract_event_queue","Events": ["s3:ObjectCreated:*"]}]}'
+aws --endpoint-url=http://127.0.0.1:4566 s3api put-bucket-notification-configuration --bucket wmt-worker --notification-configuration '{"QueueConfigurations":[{"QueueArn":"arn:aws:sqs:eu-west-2:000000000000:s3_extract_event_queue","Events": ["s3:ObjectCreated:*"]}]}'
 
-aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket wmt-worker-dashboard --region ${AWS_DEFAULT_REGION} --create-bucket-configuration LocationConstraint=${AWS_DEFAULT_REGION}
+aws --endpoint-url=http://127.0.0.1:4566 s3api create-bucket --bucket wmt-worker-dashboard --region ${AWS_DEFAULT_REGION} --create-bucket-configuration LocationConstraint=${AWS_DEFAULT_REGION}
 
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name audit_event_queue
+aws --endpoint-url=http://127.0.0.1:4566 sqs create-queue --queue-name audit_event_queue
 
-aws --endpoint-url=http://localhost:4566 sns create-topic --name domain-events
+aws --endpoint-url=http://127.0.0.1:4566 sns create-topic --name domain-events
 
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name domain_event_queue
+aws --endpoint-url=http://127.0.0.1:4566 sqs create-queue --queue-name domain_event_queue
 
-aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:eu-west-2:000000000000:domain-events --protocol sqs --notification-endpoint http://localhost:4566/000000000000/domain_event_queue --attributes '{"FilterPolicy":"{\"eventType\":[\"staff.available.hours.changed\"]}", "RawMessageDelivery": "true"}'
+aws --endpoint-url=http://127.0.0.1:4566 sns subscribe --topic-arn arn:aws:sns:eu-west-2:000000000000:domain-events --protocol sqs --notification-endpoint arn:aws:sqs:eu-west-2:000000000000:domain_event_queue --attributes '{"FilterPolicy":"{\"eventType\":[\"staff.available.hours.changed\"]}", "RawMessageDelivery": "true"}'
 
 
 echo "S3 created bucket"
 
 # to put a file using command line
-# aws --endpoint-url=http://localhost:4566 s3api put-object --bucket wmt-worker --key extract/WMP_PS.xlsx --body test/integration/resources/WMP_PS.xlsx
+# aws --endpoint-url=http://127.0.0.1:4566 s3api put-object --bucket wmt-worker --key extract/WMP_PS.xlsx --body test/integration/resources/WMP_PS.xlsx
