ba0e65309c205fe6c58cdcbe81efbf98c3d06c38
Andy Sweilam <104897795+AndySweilam@users.noreply.github.com>
2023-08-29 16:17:03 +0100

    Wfp 2086 (#619)
    * WFP-2086: update dev irsa values
    
    * WFP-2086: update Preprod irsa values
    
    * WFP-2086: clean up env values post IRSA.
    
    * WFP-2086: remove keys/secrets
    
    * Revert "WFP-2086: remove keys/secrets"
    
    This reverts commit f206b1e663d7e2789ead0c974487f02e337c4a58.
    
    * Revert "WFP-2086: clean up env values post IRSA."
    
    This reverts commit 472547e143fa34508645747ea740b230d410d923.
    
    * WFP-2086: change keys configuration for IRSA
    
    * Revert "WFP-2086: change keys configuration for IRSA"
    
    This reverts commit 4ccf3ea715d835c974003f87f02a3c5a8c03add9.
    
    * WFP-2086: amend variables
    
    * Revert "WFP-2086: amend variables"
    
    This reverts commit 6c95f6c879dfcfa21e9cf1b6754225ac8d2fba5b.
    
    * WFP-2086: remove prod keys/secrets
    
    * WFP-2086: remove prod keys/secrets
    
    * WFP-2086: change accessKeys/secrets to undefined in PROD
    
    * WFP-2086: change s3-client config
    
    * WFP-2086: change SQS config
    
    * WFP-2086: shorten credentials
    
    * WFP-2086: change sns credentials
    
    ---------
    
    Co-authored-by: AndySweilam <AndySweilam@users.noreply.github.com>
    Co-authored-by: katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>

--- app/services/aws/s3/get-s3-client.js
+++ app/services/aws/s3/get-s3-client.js
@@ -1,12 +1,18 @@
 const { S3Client } = require('@aws-sdk/client-s3')
 
 module.exports = function (config) {
+  const accessKeyId = config.accessKeyId
+  const secretAccessKey = config.secretAccessKey
+  const credentials =
+    (config.accessKeyId && config.secretAccessKey)
+      ? {
+          accessKeyId,
+          secretAccessKey
+        }
+      : undefined
   return new S3Client({
     region: config.region,
-    credentials: {
-      accessKeyId: config.accessKeyId,
-      secretAccessKey: config.secretAccessKey
-    },
+    credentials,
     endpoint: config.endpoint,
     forcePathStyle: true
   })
--- app/services/aws/sns/get-sns-client.js
+++ app/services/aws/sns/get-sns-client.js
@@ -1,12 +1,18 @@
 const { SNSClient } = require('@aws-sdk/client-sns')
 
 module.exports = function (config) {
+  const accessKeyId = config.accessKeyId
+  const secretAccessKey = config.secretAccessKey
+  const credentials =
+    (config.accessKeyId && config.secretAccessKey)
+      ? {
+          accessKeyId,
+          secretAccessKey
+        }
+      : undefined
   return new SNSClient({
     region: config.region,
-    credentials: {
-      accessKeyId: config.accessKeyId,
-      secretAccessKey: config.secretAccessKey
-    },
+    credentials,
     endpoint: config.endpoint
   })
 }
