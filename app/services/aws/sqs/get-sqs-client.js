92ff8a972b2d093f4f9661750475face7f172d09
AndySweilam <AndySweilam@users.noreply.github.com>
2023-08-29 16:10:49 +0100

    WFP-2086: change SQS config

--- app/services/aws/sqs/get-sqs-client.js
+++ app/services/aws/sqs/get-sqs-client.js
@@ -1,12 +1,18 @@
 const { SQSClient } = require('@aws-sdk/client-sqs')
 
 module.exports = function (config) {
+  const accessKeyId = config.accessKeyId
+  const secretAccessKey = config.secretAccessKey
+  const credentials =
+    (config.accessKeyId && config.secretAccessKey)
+      ? {
+          accessKeyId,
+          secretAccessKey
+        }
+      : undefined
   return new SQSClient({
     region: config.region,
-    credentials: {
-      accessKeyId: config.accessKeyId,
-      secretAccessKey: config.secretAccessKey
-    },
+    credentials: credentials,
     endpoint: config.endpoint
   })
 }
