5ee00c4f16eaa5a60ca5b60bd4335295149d62b8
Andy Sweilam <104897795+AndySweilam@users.noreply.github.com>
2023-08-29 15:37:37 +0100

    Wfp 2086 (#618)
    * WFP-2086: update dev irsa values
    
    * WFP-2086: update Preprod irsa values
    
    * WFP-2086: clean up env values post IRSA.
    
    * WFP-2086: remove keys/secrets
    
    * Revert "WFP-2086: remove keys/secrets"
    
    This reverts commit f206b1e663d7e2789ead0c974487f02e337c4a58.
    
    * Revert "WFP-2086: clean up env values post IRSA."
    
    This reverts commit 472547e143fa34508645747ea740b230d410d923.
    
    * WFP-2086: change keys configuration for IRSA
    
    * Revert "WFP-2086: change keys configuration for IRSA"
    
    This reverts commit 4ccf3ea715d835c974003f87f02a3c5a8c03add9.
    
    * WFP-2086: amend variables
    
    * Revert "WFP-2086: amend variables"
    
    This reverts commit 6c95f6c879dfcfa21e9cf1b6754225ac8d2fba5b.
    
    * WFP-2086: remove prod keys/secrets
    
    * WFP-2086: remove prod keys/secrets
    
    * WFP-2086: change accessKeys/secrets to undefined in PROD
    
    ---------
    
    Co-authored-by: AndySweilam <AndySweilam@users.noreply.github.com>
    Co-authored-by: katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ module.exports = {
   DASHBOARD_BUCKET: process.env.WMT_DASHBOARD_BUCKET || 'wmt-worker-dashboard',
   DASHBOARD_REGION: process.env.S3_REGION || 'eu-west-2',
   S3_ENDPOINT: production ? null : 'http://127.0.0.1:4566',
-  DASHBOARD_S3_ACCESS_KEY_ID: process.env.DASHBOARD_S3_ACCESS_KEY_ID || 'foobar',
-  DASHBOARD_S3_SECRET_ACCESS_KEY: process.env.DASHBOARD_S3_SECRET_ACCESS_KEY || 'foobar',
+  DASHBOARD_S3_ACCESS_KEY_ID: production ? undefined : 'foobar',
+  DASHBOARD_S3_SECRET_ACCESS_KEY: production ? undefined : 'foobar',
 
   APPINSIGHTS_INSTRUMENTATIONKEY: process.env.APPINSIGHTS_INSTRUMENTATIONKEY,
 
