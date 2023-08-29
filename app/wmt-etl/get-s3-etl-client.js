be6c75e8b371ff03822f84df1d1d1fd6523086f6
Andy Sweilam <104897795+AndySweilam@users.noreply.github.com>
2023-08-29 14:23:08 +0100

    Wfp 2086 (#617)
    * WFP-2086: update dev irsa values
    
    * WFP-2086: update Preprod irsa values
    
    * WFP-2086: clean up env values post IRSA.
    
    * WFP-2086: remove keys/secrets
    
    * Revert "WFP-2086: remove keys/secrets"
    
    This reverts commit f206b1e663d7e2789ead0c974487f02e337c4a58.
    
    * Revert "WFP-2086: clean up env values post IRSA."
    
    This reverts commit 472547e143fa34508645747ea740b230d410d923.
    
    * WFP-2086: change keys configuration for IRSA
    
    * Revert "WFP-2086: change keys configuration for IRSA"
    
    This reverts commit 4ccf3ea715d835c974003f87f02a3c5a8c03add9.
    
    * WFP-2086: amend variables
    
    * Revert "WFP-2086: amend variables"
    
    This reverts commit 6c95f6c879dfcfa21e9cf1b6754225ac8d2fba5b.
    
    * WFP-2086: remove prod keys/secrets
    
    * WFP-2086: remove prod keys/secrets
    
    ---------
    
    Co-authored-by: AndySweilam <AndySweilam@users.noreply.github.com>
    Co-authored-by: katie-cawthorne <katie.cawthorne@digital.justice.gov.uk>

--- app/wmt-etl/get-s3-etl-client.js
+++ app/wmt-etl/get-s3-etl-client.js
@@ -4,8 +4,8 @@ const getS3Client = require('../services/aws/s3/get-s3-client')
 
 const s3Client = getS3Client({
   region: S3.REGION,
-  accessKeyId: S3.ACCESS_KEY_ID,
-  secretAccessKey: S3.SECRET_ACCESS_KEY,
+  accessKeyId: S3.ACCESS_KEY_ID ? S3.ACCESS_KEY_ID : null,
+  secretAccessKey: S3.SECRET_ACCESS_KEY ? S3.SECRET_ACCESS_KEY : null,
   endpoint: S3.ENDPOINT
 })
 
