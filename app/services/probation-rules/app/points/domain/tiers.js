1a9c13caf5cb3f6a84670c18f006858edfbc09a4
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2024-04-05 14:09:02 +0100

    Wfp 2438 update workload logic (#650)
    * WFP-2438-workload-logic-first-commit
    
    * WFP-2438-workload-logic-wip-integration-tests-failing
    
    * WFP-2438-workload-logic-wip-integration-tests-passing

--- app/services/probation-rules/app/points/domain/tiers.js
+++ app/services/probation-rules/app/points/domain/tiers.js
@@ -4,7 +4,8 @@ const TierCounts = require('./tier-counts')
 
 // WMT0229 Change needed here when extract column names are known
 class Tiers {
-  constructor (location, untiered, a3, a2, a1, a0, b3, b2, b1, b0, c3, c2, c1, c0, d3, d2, d1, d0, total) {
+  constructor (location, untiered, a3, a2, a1, a0, b3, b2, b1, b0, c3, c2, c1, c0, d3, d2, d1, d0,
+    a3s, a2s, a1s, a0s, b3s, b2s, b1s, b0s, c3s, c2s, c1s, c0s, d3s, d2s, d1s, d0s, total) {
     this.location = location // Community, Custody or Licence/License
     this.untiered = untiered // Tier 0
     this.a3 = a3 // Tier 1
@@ -23,6 +24,22 @@ class Tiers {
     this.d2 = d2 // Tier 14
     this.d1 = d1 // Tier 15
     this.d0 = d0 // Tier 16
+    this.a3s = a3s // Tier 17
+    this.a2s = a2s // Tier 18
+    this.a1s = a1s // Tier 19
+    this.a0s = a0s // Tier 20
+    this.b3s = b3s // Tier 21
+    this.b2s = b2s // Tier 22
+    this.b1s = b1s // Tier 23
+    this.b0s = b0s // Tier 24
+    this.c3s = c3s // Tier 25
+    this.c2s = c2s // Tier 26
+    this.c1s = c1s // Tier 27
+    this.c0s = c0s // Tier 28
+    this.d3s = d3s // Tier 29
+    this.d2s = d2s // Tier 30
+    this.d1s = d1s // Tier 31
+    this.d0s = d0s // Tier 32
     this.total = total
     this.isValid()
   }
@@ -48,6 +65,26 @@ class Tiers {
     assertObjectType(this.d1, TierCounts, 'TierCounts d1')
     assertObjectType(this.d0, TierCounts, 'TierCounts d0')
 
+    assertObjectType(this.a3s, TierCounts, 'TierCounts a3s')
+    assertObjectType(this.a2s, TierCounts, 'TierCounts a2s')
+    assertObjectType(this.a1s, TierCounts, 'TierCounts a1s')
+    assertObjectType(this.a0s, TierCounts, 'TierCounts a0s')
+
+    assertObjectType(this.b3s, TierCounts, 'TierCounts b3s')
+    assertObjectType(this.b2s, TierCounts, 'TierCounts b2s')
+    assertObjectType(this.b1s, TierCounts, 'TierCounts b1s')
+    assertObjectType(this.b0s, TierCounts, 'TierCounts b0s')
+
+    assertObjectType(this.c3s, TierCounts, 'TierCounts c3s')
+    assertObjectType(this.c2s, TierCounts, 'TierCounts c2s')
+    assertObjectType(this.c1s, TierCounts, 'TierCounts c1s')
+    assertObjectType(this.c0s, TierCounts, 'TierCounts c0s')
+
+    assertObjectType(this.d3s, TierCounts, 'TierCounts d3s')
+    assertObjectType(this.d2s, TierCounts, 'TierCounts d2s')
+    assertObjectType(this.d1s, TierCounts, 'TierCounts d1s')
+    assertObjectType(this.d0s, TierCounts, 'TierCounts d0s')
+
     assertObjectType(this.untiered, TierCounts, 'TierCounts untiered')
     assertLocation(this.location, 'location')
   }
@@ -70,6 +107,22 @@ class Tiers {
       this.d2, // Tier 14
       this.d1, // Tier 15
       this.d0, // Tier 16
+      this.a3s, // Tier 17
+      this.a2s, // Tier 18
+      this.a1s, // Tier 19
+      this.a0s, // Tier 20
+      this.b3s, // Tier 21
+      this.b2s, // Tier 22
+      this.b1s, // Tier 23
+      this.b0s, // Tier 24
+      this.c3s, // Tier 25
+      this.c2s, // Tier 26
+      this.c1s, // Tier 27
+      this.c0s, // Tier 28
+      this.d3s, // Tier 29
+      this.d2s, // Tier 30
+      this.d1s, // Tier 31
+      this.d0s, // Tier 32
       this.untiered // Tier 0
     ]
     return list
