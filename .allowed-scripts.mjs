ec597b3f4582b8cd622185dbcde6db83bdc1306a
wood-n <natalie.wood@digital.justice.gov.uk>
2026-02-17 11:30:16 +0000

    add and configure hmpps-npm-script-allowlist

---
+++ .allowed-scripts.mjs
@@ -0,0 +1,8 @@
+import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+
+export default configureAllowedScripts({
+   allowlist: {
+      "node_modules/dtrace-provider@0.8.8": "ALLOW",
+      "node_modules/fsevents@2.3.2": "ALLOW"
+   },
+})
