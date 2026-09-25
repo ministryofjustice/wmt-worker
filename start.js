a072b82e4a433bbfc0b253e78e2544f96bdd0b8a
Joe Clay <joe.clay@justice.gov.uk>
2026-09-25 16:43:09 +0100

    Disable cron jobs entirely (#808)

--- start.js
+++ start.js
@@ -3,13 +3,13 @@ const { initialiseAppInsights } = require('./app/services/azure-appinsights')
 initialiseAppInsights()
 
 const childProcess = require('child_process')
-const extractListener = require('./app/wmt-etl/extract-files-listener')
-const taskCronJob = require('./app/task-cron-job')
+// const extractListener = require('./app/wmt-etl/extract-files-listener')
+// const taskCronJob = require('./app/task-cron-job')
 const updateInProgressTasksToPending = require('./app/services/data/update-in-progress-tasks-to-pending')
 
 updateInProgressTasksToPending().then(function () {
-  taskCronJob.start()
-  extractListener.start()
+  // taskCronJob.start()
+  // extractListener.start()
 
   childProcess.fork('start-server.js')
 })
